dbed101ca5cb878897c20e88d8797e1e4fc89ed8
Robin Dunn <robin@alldunn.com>
2010-12-27 19:03:48 +0000

    add some more testing code
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@66468 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/simple/events.py
+++ samples/simple/events.py
@@ -1,16 +1,28 @@
 
 import wxPhoenix as wx
+##import os; print 'PID:', os.getpid(); raw_input('Ready to start, press enter...')
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kw):
         wx.Frame.__init__(self, *args, **kw)
         self.Bind(wx.EVT_SIZE, self.onSize)
-        
+        wx.CallAfter(self.after, 1, 2, 3)
+    def after(self, a, b, c):
+        print 'Called via wx.CallAfter:', a, b, c
     def onSize(self, evt):
         print repr(evt.Size)
+        evt.Skip()
         
-app = wx.App()
-frm = MyFrame(None, title="Hello Events", size=(480,360))
-frm.Show()
+class MyApp(wx.App):
+    def OnInit(self):
+        print 'OnInit'
+        frm = MyFrame(None, title="Hello with Events", size=(480,360))
+        frm.Show()
+        return True
+    def OnExit(self):
+        print 'OnExit'
+        return 0
+        
+app = MyApp()
 app.MainLoop()
 
--- samples/simple/hello.py
+++ samples/simple/hello.py
@@ -1,7 +1,6 @@
-import sys, os
 import wxPhoenix as wx
 
-#print 'PID:', os.getpid(); raw_input('Ready to start, press enter...')
+#import os; print 'PID:', os.getpid(); raw_input('Ready to start, press enter...')
  
 app = wx.App()
 frm = wx.Frame(None, title="Hello World!")
