04f10c5868822146aa65a5ea43f154902abc1b68
Robin Dunn <robin@alldunn.com>
2011-09-03 01:52:23 +0000

    Update Phoenix copyright statements to 2011
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@68978 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- etg/tooltip.py
+++ etg/tooltip.py
@@ -3,7 +3,7 @@
 # Author:      Robin Dunn
 #
 # Created:     30-Nov-2010
-# Copyright:   (c) 2010 by Total Control Software
+# Copyright:   (c) 2011 by Total Control Software
 # License:     wxWindows License
 #---------------------------------------------------------------------------
 
