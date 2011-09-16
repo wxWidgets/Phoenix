ad305b513be86c585bc41db8f833c134316f0220
Kevin Ollivier <kevino@theolliviers.com>
2011-09-16 20:49:42 +0000

    Add missing filedlg.py.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@69103 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ etg/filedlg.py
@@ -0,0 +1,49 @@
+#---------------------------------------------------------------------------
+# Name:        etg/filedlg.py
+# Author:      Kevin Ollivier
+#
+# Created:     10-Sept-2011
+# Copyright:   (c) 2011 by Kevin Ollivier
+# License:     wxWindows License
+#---------------------------------------------------------------------------
+
+import etgtools
+import etgtools.tweaker_tools as tools
+
+PACKAGE   = "wx"
+MODULE    = "_core"
+NAME      = "filedlg"   # Base name of the file to generate to for this script
+DOCSTRING = ""
+
+# The classes and/or the basename of the Doxygen XML files to be processed by
+# this script. 
+ITEMS  = [ 'wxFileDialog' ]
+    
+#---------------------------------------------------------------------------
+
+def run():
+    # Parse the XML file(s) building a collection of Extractor objects
+    module = etgtools.ModuleDef(PACKAGE, MODULE, NAME, DOCSTRING)
+    etgtools.parseDoxyXML(module, ITEMS)
+    
+    #-----------------------------------------------------------------
+    # Tweak the parsed meta objects in the module object as needed for
+    # customizing the generated code and docstrings.
+    
+    c = module.find('wxFileDialog')
+    
+    # TODO: add this back?
+    c.find('SetExtraControlCreator').ignore()
+
+    tools.fixWindowClass(c)
+    
+    #-----------------------------------------------------------------
+    tools.doCommonTweaks(module)
+    tools.addGetterSetterProps(module)
+    tools.runGenerators(module)
+    
+    
+#---------------------------------------------------------------------------
+if __name__ == '__main__':
+    run()
+
