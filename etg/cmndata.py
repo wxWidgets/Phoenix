7e2835ea61fad5a1fe602453117b59a01a640d12
Robin Dunn <robin@alldunn.com>
2011-10-02 05:33:22 +0000

    Add other classes from cmndata.h, add some extra methods and such we had in Classic, add unit tests
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@69275 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- etg/cmndata.py
+++ etg/cmndata.py
@@ -17,14 +17,16 @@ DOCSTRING = ""
 
 # The classes and/or the basename of the Doxygen XML files to be processed by
 # this script. 
-ITEMS  = [ 'wxPrintData', ]    
+ITEMS  = [ 'wxPageSetupDialogData',
+           'wxPrintData', 
+           'wxPrintDialogData',
+           ]    
     
 #---------------------------------------------------------------------------
 
 def run():
     # Parse the XML file(s) building a collection of Extractor objects
     module = etgtools.ModuleDef(PACKAGE, MODULE, NAME, DOCSTRING)
-    #module.items.append(etgtools.TypedefDef(type='int', name='wxPrintQuality'))
     
     etgtools.parseDoxyXML(module, ITEMS)
     
@@ -32,6 +34,73 @@ def run():
     # Tweak the parsed meta objects in the module object as needed for
     # customizing the generated code and docstrings.
     
+    c = module.find('wxPageSetupDialogData')
+    assert isinstance(c, etgtools.ClassDef)
+    tools.removeVirtuals(c)
+    
+    c.find('GetPrintData').overloads = []
+    
+    c.addCppMethod('int', '__nonzero__', '()', """\
+        return self->IsOk();
+        """)
+    
+    c.addProperty('MarginBottomRight', 'GetMarginBottomRight', 'SetMarginBottomRight')
+    c.addProperty('MarginTopLeft', 'GetMarginTopLeft', 'SetMarginTopLeft')
+    c.addProperty('MinMarginBottomRight', 'GetMinMarginBottomRight', 'SetMinMarginBottomRight')
+    c.addProperty('MinMarginTopLeft', 'GetMinMarginTopLeft', 'SetMinMarginTopLeft')
+    c.addProperty('PaperId', 'GetPaperId', 'SetPaperId')
+    c.addProperty('PaperSize', 'GetPaperSize', 'SetPaperSize')
+    c.addProperty('PrintData', 'GetPrintData', 'SetPrintData')
+
+
+
+    c = module.find('wxPrintData')
+    assert isinstance(c, etgtools.ClassDef)
+    tools.removeVirtuals(c)
+
+    c.addCppMethod('int', '__nonzero__', '()', """\
+        return self->IsOk();
+        """)
+    
+    # TODO: These two methods should use something other than a PyString for
+    # holding the data...
+    c.addCppMethod('PyObject*', 'GetPrivData', '()', """\
+        PyObject* data;
+        wxPyBlock_t blocked = wxPyBeginBlockThreads();
+        data = PyString_FromStringAndSize(self->GetPrivData(),
+                                          self->GetPrivDataLen());
+        wxPyEndBlockThreads(blocked);
+        return data;    
+        """)
+    
+    c.addCppMethod('void', 'SetPrivData', '(PyObject* data)', """\
+        if (! PyString_Check(data)) {
+            wxPyBLOCK_THREADS(PyErr_SetString(PyExc_TypeError,
+                                              "Expected string object"));
+            return /* NULL */ ;
+        }
+
+        wxPyBlock_t blocked = wxPyBeginBlockThreads();
+        self->SetPrivData(PyString_AS_STRING(data), PyString_GET_SIZE(data));
+        wxPyEndBlockThreads(blocked);
+        """)
+    
+    c.addGetterSetterProps()
+    
+
+
+    c = module.find('wxPrintDialogData')
+    assert isinstance(c, etgtools.ClassDef)
+    tools.removeVirtuals(c)
+    c.find('SetSetupDialog').ignore()
+
+    c.addCppMethod('int', '__nonzero__', '()', """\
+        return self->IsOk();
+        """)
+    
+    c.addGetterSetterProps()
+
+
     #-----------------------------------------------------------------
     tools.doCommonTweaks(module)
     tools.runGenerators(module)
---
+++ unittests/test_cmndata.py
@@ -0,0 +1,47 @@
+import imp_unittest, unittest
+import wtc
+import wx
+
+#---------------------------------------------------------------------------
+
+class cmndata_tests(wtc.WidgetTestCase):
+    
+    def test_cmndataClassConstructors(self):
+        psdd1 = wx.PageSetupDialogData()
+        pd1 = wx.PrintData()
+        pdd1 = wx.PrintDialogData()
+        
+        psdd2 = wx.PageSetupDialogData(pd1)
+        psdd3 = wx.PageSetupDialogData(psdd2)
+        
+        pd2 = wx.PrintData(pd1)
+        
+        pdd2 = wx.PrintDialogData(pdd1)
+        pdd3 = wx.PrintDialogData(pd1)
+        
+    
+    
+    def test_cppMethods(self):
+        pd = wx.PrintData()
+        data = pd.GetPrivData()
+        pd.SetPrivData(data)
+        
+        # property for the same methods
+        data = pd.PrivData
+        pd.PrivData = data
+
+
+    def test_nonzero(self):
+        psdd = wx.PageSetupDialogData()
+        pd = wx.PrintData()
+        pdd = wx.PrintDialogData()
+    
+        psdd.__nonzero__()
+        pd.__nonzero__()
+        pdd.__nonzero__()
+        
+#---------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    unittest.main()
