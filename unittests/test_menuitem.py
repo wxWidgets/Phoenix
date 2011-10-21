e9c9ba13f4063d82390105a263fc56b3e652841b
Robin Dunn <robin@alldunn.com>
2011-10-21 08:04:22 +0000

    Provide implementations for the MSW-only methods, add unittests
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@69496 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ unittests/test_menuitem.py
@@ -0,0 +1,48 @@
+import imp_unittest, unittest
+import wtc
+import wx
+import os
+
+pngFile = os.path.join(os.path.dirname(__file__), 'pointy.png')
+
+#---------------------------------------------------------------------------
+
+class menuitem_Tests(wtc.WidgetTestCase):
+
+    def test_menuitemCtor(self):
+        m1 = wx.MenuItem()
+        m2 = wx.MenuItem(None, -1, "Menu Item", "Help text", wx.ITEM_NORMAL)
+        m2.SetBitmap(wx.Bitmap(pngFile))
+        
+        
+    def test_menuitemProperties(self):
+        m = wx.MenuItem()
+        m.BackgroundColour
+        m.Bitmap
+        m.Font
+        m.Help
+        m.Id
+        m.ItemLabel
+        m.ItemLabelText
+        m.Kind
+        m.Label
+        m.MarginWidth
+        m.Menu
+        m.Name
+        m.SubMenu
+        m.Text
+        m.TextColour
+        
+        
+    def test_menuitemKinds(self):
+        wx.ITEM_CHECK
+        wx.ITEM_DROPDOWN
+        wx.ITEM_NORMAL
+        wx.ITEM_RADIO
+        wx.ITEM_SEPARATOR
+        
+#---------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    unittest.main()
