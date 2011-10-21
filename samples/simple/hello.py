f738327be2276f199c241a5bde23c4a84eacedb1
Robin Dunn <robin@alldunn.com>
2011-10-21 21:10:23 +0000

    We can do lots more than just an app and a frame these days, so let's do more in our Hello World sample.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@69506 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/simple/hello.py
+++ samples/simple/hello.py
@@ -4,6 +4,12 @@ print wx.version()
  
 app = wx.App()
 frm = wx.Frame(None, title="Hello World!")
+
+pnl = wx.Panel(frm)
+pnl.BackgroundColour = 'sky blue'
+st = wx.StaticText(pnl, -1, 'This is wxPython\n%s' % wx.version(), (15,15))
+st.SetFont(wx.Font(14, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
+
 frm.Show()
 app.MainLoop()
 
