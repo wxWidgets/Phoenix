b0192d8b09c545ede3e4577ae5b22b24680e6e55
Robin Dunn <robin@alldunn.com>
2011-11-02 03:43:50 +0000

    Add spinbutt module containing wxSpinButton and wxSpinEvent
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@69636 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ etg/spinbutt.py
@@ -0,0 +1,61 @@
+#---------------------------------------------------------------------------
+# Name:        etg/spinbutt.py
+# Author:      Robin Dunn
+#
+# Created:     01-Nov-2011
+# Copyright:   (c) 2011 by Total Control Software
+# License:     wxWindows License
+#---------------------------------------------------------------------------
+
+import etgtools
+import etgtools.tweaker_tools as tools
+
+PACKAGE   = "wx"   
+MODULE    = "_core"
+NAME      = "spinbutt"   # Base name of the file to generate to for this script
+DOCSTRING = ""
+
+# The classes and/or the basename of the Doxygen XML files to be processed by
+# this script. 
+ITEMS  = [ "wxSpinButton",
+           "wxSpinEvent",
+           ]    
+
+#---------------------------------------------------------------------------
+
+def run():
+    # Parse the XML file(s) building a collection of Extractor objects
+    module = etgtools.ModuleDef(PACKAGE, MODULE, NAME, DOCSTRING)
+    etgtools.parseDoxyXML(module, ITEMS)
+    
+    #-----------------------------------------------------------------
+    # Tweak the parsed meta objects in the module object as needed for
+    # customizing the generated code and docstrings.
+    
+    c = module.find('wxSpinButton')
+    assert isinstance(c, etgtools.ClassDef)
+    
+    c.addPyMethod('GetRange', '(self)', 'return (self.GetMin(), self.GetMax())')
+    c.addPyMethod('SetMin', '(self, minVal)', 'self.SetRange(minVal, self.GetMax())')
+    c.addPyMethod('SetMax', '(self, maxVal)', 'self.SetRange(self.GetMin(), maxVal)')
+    c.addPyCode("""\
+        EVT_SPIN_UP   = wx.PyEventBinder( wxEVT_SPIN_UP, 1)
+        EVT_SPIN_DOWN = wx.PyEventBinder( wxEVT_SPIN_DOWN, 1)
+        EVT_SPIN      = wx.PyEventBinder( wxEVT_SPIN, 1)
+        """)
+
+    tools.fixWindowClass(c)
+    
+    c = module.find('wxSpinEvent')
+    tools.fixEventClass(c)
+    
+    
+    #-----------------------------------------------------------------
+    tools.doCommonTweaks(module)
+    tools.runGenerators(module)
+    
+    
+#---------------------------------------------------------------------------
+if __name__ == '__main__':
+    run()
+
