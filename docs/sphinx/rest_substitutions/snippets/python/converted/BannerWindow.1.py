8c1db7a9943c97db1cb9d7c6a3c174747568bb88
Andrea Gavana <andrea.gavana@gmail.com>
2012-05-22 19:27:06 +0000

    Phoenix:
    - Add the BannerWindow sample to the snippets;
    - Add the animationctrl.png images for the various platforms.
    
    I'm running out of space at the xoomer domain :-(
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@71537 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ docs/sphinx/rest_substitutions/snippets/python/converted/BannerWindow.1.py
@@ -0,0 +1,23 @@
+
+        class MyFrame(wx.Frame):
+
+            def __init__(self, parent):
+
+                # ... create the frame itself ...
+                wx.Frame.__init__(self, parent)
+            
+                # Create and initialize the banner.
+                banner = wx.BannerWindow(self, wx.TOP)
+                banner.SetText("Welcome to my wonderful program",
+                               "  Before doing anything else, you need to connect to "
+                               "the online server.\n"
+                               "  Please enter your credentials in the controls below.")
+
+                # And position it along the left edge of the window.
+                sizer = wx.BoxSizer(wx.VERTICAL)
+                sizer.Add(banner, wx.SizerFlags().Expand())
+
+                # ... add the rest of the window contents to the same sizer ...
+
+                self.SetSizerAndFit(sizer)
+
