ef79b8c371340d10e06ad6d0778fe32229264808
Andrea Gavana <andrea.gavana@gmail.com>
2012-05-26 20:35:44 +0000

    - Shorten up the documentation page header; - Add samples for wx.SplashScreen and AffineMatrix2D.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@71573 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

---
+++ docs/sphinx/rest_substitutions/snippets/python/converted/SplashScreen.1.py
@@ -0,0 +1,8 @@
+    
+        bitmap = wx.Bitmap('splash16.png', wx.BITMAP_TYPE_PNG)
+
+        splash = wx.SplashScreen(bitmap, wx.SPLASH_CENTRE_ON_SCREEN|wx.SPLASH_TIMEOUT,
+                               6000, None, -1, wx.DefaultPosition, wx.DefaultSize,
+                               wx.BORDER_SIMPLE|wx.STAY_ON_TOP)
+
+        wx.Yield()
