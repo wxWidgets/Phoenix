a775511afddebd7efd93bf6d82b39050d4a37ddc
Robin Dunn <robin@alldunn.com>
2012-07-10 05:15:23 +0000

    Add some tests for HeaderCtrl
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@71992 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- unittests/test_headerctrl.py
+++ unittests/test_headerctrl.py
@@ -6,10 +6,61 @@ import wx
 
 class headerctrl_Tests(wtc.WidgetTestCase):
 
-    # TODO: Remove this test and add real ones.
     def test_headerctrl1(self):
-        self.fail("Unit tests for headerctrl not implemented yet.")
+        wx.HD_ALLOW_REORDER
+        wx.HD_ALLOW_HIDE
+        wx.HD_DEFAULT_STYLE
         
+        wx.wxEVT_COMMAND_HEADER_CLICK
+        wx.wxEVT_COMMAND_HEADER_RIGHT_CLICK
+        wx.wxEVT_COMMAND_HEADER_MIDDLE_CLICK
+        wx.wxEVT_COMMAND_HEADER_DCLICK
+        wx.wxEVT_COMMAND_HEADER_RIGHT_DCLICK
+        wx.wxEVT_COMMAND_HEADER_MIDDLE_DCLICK
+        wx.wxEVT_COMMAND_HEADER_SEPARATOR_DCLICK
+        wx.wxEVT_COMMAND_HEADER_BEGIN_RESIZE
+        wx.wxEVT_COMMAND_HEADER_RESIZING
+        wx.wxEVT_COMMAND_HEADER_END_RESIZE
+        wx.wxEVT_COMMAND_HEADER_BEGIN_REORDER
+        wx.wxEVT_COMMAND_HEADER_END_REORDER
+        wx.wxEVT_COMMAND_HEADER_DRAGGING_CANCELLED
+        
+        wx.EVT_HEADER_CLICK
+        wx.EVT_HEADER_RIGHT_CLICK
+        wx.EVT_HEADER_MIDDLE_CLICK
+        wx.EVT_HEADER_DCLICK
+        wx.EVT_HEADER_RIGHT_DCLICK
+        wx.EVT_HEADER_MIDDLE_DCLICK
+        wx.EVT_HEADER_SEPARATOR_DCLICK
+        wx.EVT_HEADER_BEGIN_RESIZE
+        wx.EVT_HEADER_RESIZING
+        wx.EVT_HEADER_END_RESIZE
+        wx.EVT_HEADER_BEGIN_REORDER
+        wx.EVT_HEADER_END_REORDER
+        wx.EVT_HEADER_DRAGGING_CANCELLED
+        
+    @unittest.expectedFailure  # it's an abstract class
+    def test_headerctrl2(self):
+        hc = wx.HeaderCtrl(self.frame)
+        
+    def test_headerctrl3(self):
+        hc = wx.HeaderCtrlSimple()
+        hc.Create(self.frame)
+    
+    def test_headerctrl4(self):
+        hc = wx.HeaderCtrlSimple(self.frame)
+    
+        col = wx.HeaderColumnSimple("Hello")
+        hc.AppendColumn(col)
+    
+        col = wx.HeaderColumnSimple("World")
+        hc.AppendColumn(col)
+
+
+    def test_headerctrl4(self):
+        evt = wx.HeaderCtrlEvent()
+
+
 #---------------------------------------------------------------------------
 
 if __name__ == '__main__':
