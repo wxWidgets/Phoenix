b265aa59f5ac3ff7ab1411fd571ecd9fb4300cce
Robin Dunn <robin@alldunn.com>
2012-07-12 23:59:58 +0000

    Display the bitness of the python used to run the sample.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@72060 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/simple/hello.py
+++ samples/simple/hello.py
@@ -1,17 +1,17 @@
-import sys, os, wx
+import sys, os, platform, wx
 print(wx.version()); print(sys.version)
 
 app = wx.App()
-frm = wx.Frame(None, title="Hello World!")
+frm = wx.Frame(None, title="Hello World!", size=(400,275))
 pnl = wx.Panel(frm)
 pnl.BackgroundColour = 'sky blue'
 
 st = wx.StaticText(pnl, -1, 'Hello World!', (15,10))
 st.SetFont(wx.Font(14, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
 
-st = wx.StaticText(pnl, pos=(15,40), label=
-                   'This is wxPython %s\nrunning on Python %s' % 
-                       (wx.version(), sys.version.split(' ')[0]))
+st = wx.StaticText(pnl, pos=(15,40), 
+        label='This is wxPython %s\nrunning on Python %s %s' % 
+        (wx.version(), sys.version.split(' ')[0], platform.architecture()[0]))
 st.SetFont(wx.Font(10, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
 
 bmp = wx.Bitmap(os.path.join(os.path.dirname(__file__), 'phoenix_main.png'))
