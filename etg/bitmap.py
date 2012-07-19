e7e5fe51c970049643b95a5cbfbda672054d0e43
Robin Dunn <robin@alldunn.com>
2012-07-19 06:47:30 +0000

    Add wxBitmap constructor accepting a list of strings of XPM data
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@72145 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- etg/bitmap.py
+++ etg/bitmap.py
@@ -42,10 +42,41 @@ def run():
     
     tools.removeVirtuals(c)
 
-    c.find('wxBitmap').findOverload('(const char *const *bits)').ignore()
     c.find('wxBitmap.bits').type = 'const char*'
     c.find('wxBitmap.type').default = 'wxBITMAP_TYPE_ANY'
     c.find('LoadFile.type').default = 'wxBITMAP_TYPE_ANY'
+
+    c.find('wxBitmap').findOverload('(const char *const *bits)').ignore()
+
+    c.addCppCtor('(PyObject* listOfBytes)',
+        doc="Construct a Bitmap from a list of strings formatted as XPM data.",
+        body="""\
+            wxPyThreadBlocker blocker;
+            char**    cArray = NULL;
+            int       count;
+            char      errMsg[] = "Expected a list of bytes objects.";
+            
+            if (!PyList_Check(listOfBytes)) {
+                PyErr_SetString(PyExc_TypeError, errMsg);
+                return NULL;
+            }
+            count = PyList_Size(listOfBytes);
+            cArray = new char*[count];
+
+            for(int x=0; x<count; x++) {
+                PyObject* item = PyList_GET_ITEM(listOfBytes, x);
+                if (!PyBytes_Check(item)) {
+                    PyErr_SetString(PyExc_TypeError, errMsg);
+                    delete [] cArray;
+                    return NULL;
+                }
+                cArray[x] = PyBytes_AsString(item);
+            }
+            wxBitmap* bmp = new wxBitmap(cArray);
+            delete [] cArray;
+            return bmp;
+            """)
+
 
     c.find('SetMask.mask').transfer = True
 
