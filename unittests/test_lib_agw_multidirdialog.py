824bd0f873745517e9274a800f7a52ceba3a8c1f
Robin Dunn <robin@alldunn.com>
2012-07-28 19:43:07 +0000

    Allow for an item to be already selected in the tree
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@72237 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- unittests/test_lib_agw_multidirdialog.py
+++ unittests/test_lib_agw_multidirdialog.py
@@ -16,7 +16,12 @@ class lib_agw_multidirdialog_Tests(wtc.WidgetTestCase):
         dlg = MDD.MultiDirDialog(self.frame, title="Custom MultiDirDialog",
                                  agwStyle=MDD.DD_MULTIPLE|MDD.DD_DIR_MUST_EXIST)
 
-        self.assertTrue(dlg.GetPaths() == [])
+        self.assertTrue(isinstance(dlg.GetPaths(), list))
+        
+        # it looks like the generic dir ctrl may start out with an item
+        # selected, so allow for that here
+        self.assertTrue(len(dlg.GetPaths()) in [0,1])
+        
         
     def test_lib_agw_multidirdialogConstantsExist(self):
         MDD.DD_DEFAULT_STYLE
