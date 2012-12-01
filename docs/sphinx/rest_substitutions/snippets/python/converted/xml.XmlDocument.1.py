983d3c4c3587ad6703a726878e555b0d74394505
Andrea Gavana <andrea.gavana@gmail.com>
2012-12-01 22:57:07 +0000

    Fix a couple of broken snippets for `xml` and `xrc`.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@73094 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- docs/sphinx/rest_substitutions/snippets/python/converted/xml.XmlDocument.1.py
+++ docs/sphinx/rest_substitutions/snippets/python/converted/xml.XmlDocument.1.py
@@ -1,41 +1,44 @@
-    
-        doc = wx.xml.XmlDocument()
-        if not doc.Load("myfile.xml"):
-            return False
 
-        # start processing the XML file
-        if doc.GetRoot().GetName() != "myroot-node":
-            return False
-
-        # examine prologue
-        prolog = doc.GetDocumentNode().GetChildren()
-        while prolog:
-
-            if prolog.GetType() == wx.xml.XML_PI_NODE and prolog.GetName() == "target":
-
-                # process Process Instruction contents
-                pi = prolog.GetContent()
-
-                # Other code here...
-
-        child = doc.GetRoot().GetChildren()
-        while child:
-
-            if child.GetName() == "tag1":
-
-                # process text enclosed by tag1/tag1
-                content = child.GetNodeContent()
-
-                # Other code here...
-
-                # process attributes of tag1
-                attrvalue1 = child.GetAttribute("attr1", "default-value")
-                attrvalue2 = child.GetAttribute("attr2", "default-value")
-
-             elif child.GetName() == "tag2":
-
-                # process tag2 ...
-                attrvalue3 = child.GetAttribute("attr3", "default-value")
+        def ScanDocument():
             
+            doc = wx.xml.XmlDocument()
+            if not doc.Load("myfile.xml"):
+                return False
 
-            child = child.GetNext()
+            # start processing the XML file
+            if doc.GetRoot().GetName() != "myroot-node":
+                return False
+
+            # examine prologue
+            prolog = doc.GetDocumentNode().GetChildren()
+            while prolog:
+
+                if prolog.GetType() == wx.xml.XML_PI_NODE and prolog.GetName() == "target":
+
+                    # process Process Instruction contents
+                    pi = prolog.GetContent()
+
+                    # Other code here...
+
+            child = doc.GetRoot().GetChildren()
+            while child:
+
+                if child.GetName() == "tag1":
+
+                    # process text enclosed by tag1/tag1
+                    content = child.GetNodeContent()
+
+                    # Other code here...
+
+                    # process attributes of tag1
+                    attrvalue1 = child.GetAttribute("attr1", "default-value")
+                    attrvalue2 = child.GetAttribute("attr2", "default-value")
+
+                elif child.GetName() == "tag2":
+
+                    # process tag2 ...
+                    attrvalue3 = child.GetAttribute("attr3", "default-value")
+                
+
+                child = child.GetNext()
+
