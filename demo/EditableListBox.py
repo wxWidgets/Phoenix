bd888d73d1282f3034778ad6844a6c9b993d4923
Robin Dunn <robin@alldunn.com>
2013-07-05 22:26:07 +0000

    EditableListBox Demo Phoenix Fixes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@74384 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- demo/EditableListBox.py
+++ demo/EditableListBox.py
@@ -1,6 +1,6 @@
 
 import  wx
-import  wx.gizmos   as  gizmos
+import  wx.adv
 
 #----------------------------------------------------------------------
 
@@ -9,10 +9,13 @@ class TestPanel(wx.Panel):
         wx.Panel.__init__(self, parent, -1)
         self.log = log
 
-        self.elb = gizmos.EditableListBox(
-                    self, -1, "List of Stuff", (50,50), (250, 250)
-                    )
-                    #style=wx.EL_ALLOW_NEW | wx.EL_ALLOW_EDIT | wx.EL_ALLOW_DELETE)
+        self.elb = wx.adv.EditableListBox(
+                    self, -1, "List of Stuff", (50,50), (250, 250),
+                    style=wx.adv.EL_DEFAULT_STYLE |
+                          wx.adv.EL_NO_REORDER |
+                          wx.adv.EL_ALLOW_NEW |
+                          wx.adv.EL_ALLOW_EDIT |
+                          wx.adv.EL_ALLOW_DELETE)
 
         self.elb.SetStrings(["This is a nifty ListBox widget",
                              "that is editable by the user.",
@@ -37,7 +40,7 @@ def runTest(frame, nb, log):
 overview = """\
 <html>
 <body>
-This class provides a composite control that lets the user easily enter and edit 
+This class provides a composite control that lets the user easily enter and edit
 a list of strings.
 
 <p><b>Styles supported:</b><p>
@@ -60,19 +63,19 @@ a list of strings.
 
 <p><b>Methods:</b>
 <ul>
-    <li><b>SetStrings(const wxArrayString& strings)</b> - Set an array of strings 
-    into the control. <b>Note</b>: The wxPython method accepts a Python list instead 
+    <li><b>SetStrings(const wxArrayString& strings)</b> - Set an array of strings
+    into the control. <b>Note</b>: The wxPython method accepts a Python list instead
     of an array of strings.
-            
+
     <li><b>void GetStrings(wxArrayString& strings)</b> - Retrieves an array
     of strings from the control. The wxPython version returns a list of strings.
 
     <li><b>GetListCtrl()</b> - Retrieves a reference to the actual list control
     portion of the custom control.
-    
+
     <li><b>GetDelButton()</b> - Retrieves a reference to the BitmapButton that is used
     as the 'delete' button in the control.
-    
+
     <li><b>GetNewButton()</b> - Retrieves a reference to the BitmapButton that is used
     as the 'new' button in the control.
 
@@ -81,7 +84,7 @@ a list of strings.
 
     <li><b>GetDownButton()</b> - Retrieves a reference to the BitmapButton that is used
     as the 'down' button in the control.
-    
+
     <li><b>GetEditButton()</b> - Retrieves a reference to the BitmapButton that is used
     as the 'edit' button in the control.
 </ul>
