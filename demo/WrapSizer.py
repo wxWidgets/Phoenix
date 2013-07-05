9cb40fed378aeb53ed339a4121212aff6e475bb0
Robin Dunn <robin@alldunn.com>
2013-07-05 22:32:02 +0000

    Add 2nd Orient to WrapSizer Demo
    Add VERTICAL bitmapbutton demo from 'bitmaps/bmp'
    
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@74396 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- demo/WrapSizer.py
+++ demo/WrapSizer.py
@@ -6,14 +6,14 @@ import random
 
 #----------------------------------------------------------------------
 
-class WrapSizerTest(wx.Frame):
+class WrapSizerTest1(wx.Frame):
     def __init__(self, *args, **kw):
         wx.Frame.__init__(self, *args, **kw)
 
         # Make a title area and sizer for the upper part of the panel
         pnl = wx.Panel(self)
         stx = wx.StaticText(pnl, -1, "wx.WrapSizer Layout")
-        stx.SetFont(wx.FFont(28, wx.SWISS))
+        stx.SetFont(wx.Font(28, wx.FONTFAMILY_SWISS, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
         sln = wx.StaticLine(pnl)
         upper = wx.BoxSizer(wx.VERTICAL)
         upper.Add(stx, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.TOP, 8)
@@ -22,30 +22,46 @@ class WrapSizerTest(wx.Frame):
         # In the lower part of the panel we'll put a bunch of buttons
         # in a WrapSizer
         def _makeButtons(words):
-            sizer = wx.WrapSizer()
+            wsizer = wx.WrapSizer(orient=wx.HORIZONTAL)
             for word in words:
                 btn = GenButton(pnl, -1, word, style=wx.BU_EXACTFIT)
-                sizer.Add(btn, 0, wx.ALL, 4)
-                
+                wsizer.Add(btn, 0, wx.ALL, 4)
+
                 # Add a little color to make it interesting
                 r = random.randint(128, 255)
                 g = random.randint(128, 255)
                 b = random.randint(128, 255)
                 btn.SetBackgroundColour(wx.Colour(r,g,b))
                 btn.Refresh()
-                
-            return sizer
-        
+
+            return wsizer
+
         lower1 = _makeButtons(sampleText.split())
         lower2 = _makeButtons("Try resizing this Frame to see how it works.".split())
-        
+
         pnl.Sizer = wx.BoxSizer(wx.VERTICAL)
         pnl.Sizer.Add(upper, 0, wx.EXPAND)
         pnl.Sizer.Add(lower1, 0, wx.EXPAND|wx.LEFT|wx.RIGHT, 8)
         pnl.Sizer.Add((16,16))
         pnl.Sizer.Add(lower2, 0, wx.EXPAND|wx.LEFT|wx.RIGHT, 8)
 
-        
+
+
+class WrapSizerTest2(wx.Frame):
+    def __init__(self, *args, **kw):
+        wx.Frame.__init__(self, *args, **kw)
+        panel = wx.Panel(self, -1)
+        wsizer = wx.WrapSizer(orient=wx.VERTICAL)
+        import os
+        for img16 in os.listdir('bitmaps'):
+            bmp = wx.Bitmap('bitmaps/' + img16)
+            if bmp.GetSize() == (16,16):
+                btn = wx.Button(panel, -1, size=(24,24))
+                btn.SetBitmap(bmp)
+                wsizer.Add(btn)
+        panel.SetSizer(wsizer)
+
+
 #----------------------------------------------------------------------
 
 class TestPanel(wx.Panel):
@@ -53,12 +69,19 @@ class TestPanel(wx.Panel):
         self.log = log
         wx.Panel.__init__(self, parent, -1)
 
-        b = wx.Button(self, -1, "Demonstrate wx.WrapSizer", (50,50))
-        self.Bind(wx.EVT_BUTTON, self.OnButton, b)
+        b = wx.Button(self, -1, "Demonstrate HORIZONTAL wx.WrapSizer", (50,50))
+        self.Bind(wx.EVT_BUTTON, self.OnButton1, b)
+
+        b = wx.Button(self, -1, "Demonstrate VERTICAL wx.WrapSizer", (50,100))
+        self.Bind(wx.EVT_BUTTON, self.OnButton2, b)
 
 
-    def OnButton(self, evt):
-        win = WrapSizerTest(self, -1, "wx.WrapSizer Demo", size=(640, 480))
+    def OnButton1(self, evt):
+        win = WrapSizerTest1(self, -1, "wx.WrapSizer Demo1", size=(640, 480))
+        win.Show(True)
+
+    def OnButton2(self, evt):
+        win = WrapSizerTest2(self, -1, "wx.WrapSizer Demo2", size=(640, 480))
         win.Show(True)
 
 #----------------------------------------------------------------------
