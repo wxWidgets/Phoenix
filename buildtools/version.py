7d0b8d94d073d19be83df32f977727b058b0f4b8
Robin Dunn <robin@alldunn.com>
2013-12-29 03:45:05 +0000

    Bump to 3.0.1
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@75481 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- buildtools/version.py
+++ buildtools/version.py
@@ -13,6 +13,6 @@
 
 VER_MAJOR        = 3      # The first three must match wxWidgets
 VER_MINOR        = 0
-VER_RELEASE      = 0
+VER_RELEASE      = 1
 VER_SUBREL       = 0      # wxPython release num for x.y.z release of wxWidgets
 VER_FLAGS        = ""     # release flags, such as prerelease or RC num, etc.
