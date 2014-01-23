ed90a026eabb3edf1da063ef0e02217de1403a48
Robin Dunn <robin@alldunn.com>
2014-01-23 00:19:02 +0000

    PR54: Fixes double “March” and some rst syntax issues.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@75680 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wx/lib/CDate.py
+++ wx/lib/CDate.py
@@ -24,7 +24,7 @@ _ = wx.GetTranslation
 
 Month = {0: None,
          1: _('January'), 2: _('February'), 3: _('March'),
-         4: _('March'), 5: _('May'), 6: _('June'),
+         4: _('April'), 5: _('May'), 6: _('June'),
          7: _('July'), 8: _('August'), 9: _('September'),
          10: _('October'), 11: _('November'), 12: _('December')}
 
@@ -143,9 +143,9 @@ def FromJulian(julian):
 def dayOfWeek(julian):
     """Get day of week from a julian day
 
-    param `julian`: the julian day
+    :param `julian`: the julian day
 
-    returns: the day of week as an integer and Monday = 1
+    :returns: the day of week as an integer and Monday = 1
 
     """
     return int((julian + 1) % 7)
