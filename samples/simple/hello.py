c09f6cc652e2ba1d544deea38ee147cba70244e3
Robin Dunn <robin@alldunn.com>
2014-02-06 08:04:09 +0000

    Switch back to wx.FFont
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/Phoenix/trunk@75810 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- samples/simple/hello.py
+++ samples/simple/hello.py
@@ -7,12 +7,12 @@ pnl = wx.Panel(frm)
 pnl.BackgroundColour = 'sky blue'
 
 st = wx.StaticText(pnl, -1, 'Hello World!', (15,10))
-st.SetFont(wx.Font(14, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
+st.SetFont(wx.FFont(14, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
 
 st = wx.StaticText(pnl, pos=(15,40), 
         label='This is wxPython %s\nrunning on Python %s %s' % 
         (wx.version(), sys.version.split(' ')[0], platform.architecture()[0]))
-st.SetFont(wx.Font(10, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
+st.SetFont(wx.FFont(10, wx.FONTFAMILY_SWISS, wx.FONTFLAG_BOLD))
 
 bmp = wx.Bitmap(os.path.join(os.path.dirname(__file__), 'phoenix_main.png'))
 sb = wx.StaticBitmap(pnl, label=bmp, pos=(15,85))
