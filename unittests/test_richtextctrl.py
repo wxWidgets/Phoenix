b9b6a86f65301a74157f2bc98caa866b2178a951
Robin Dunn <robin@alldunn.com>
2015-02-02 22:16:03 -0800

    richtext wip

--- unittests/test_richtextctrl.py
+++ unittests/test_richtextctrl.py
@@ -1,14 +1,40 @@
 import imp_unittest, unittest
 import wtc
 import wx
+import wx.richtext
 
 #---------------------------------------------------------------------------
 
 class richtextctrl_Tests(wtc.WidgetTestCase):
 
-    # TODO: Remove this test and add real ones.
     def test_richtextctrl1(self):
-        self.fail("Unit tests for richtextctrl not implemented yet.")
+        wx.richtext.RE_READONLY
+        wx.richtext.RE_MULTILINE
+        wx.richtext.RE_CENTER_CARET
+        wx.richtext.RE_CENTRE_CARET    
+        wx.richtext.RICHTEXT_SHIFT_DOWN
+        wx.richtext.RICHTEXT_CTRL_DOWN
+        wx.richtext.RICHTEXT_ALT_DOWN
+        wx.richtext.RICHTEXT_EX_NO_GUIDELINES
+        
+
+    def test_richtextctrl2(self):
+        info = wx.richtext.RichTextContextMenuPropertiesInfo()
+        obj = wx.richtext.RichTextParagraph()
+        info.AddItem('Name', obj)
+        
+    
+    def test_richtextctrl3(self):
+        pass
+
+    def test_richtextctrl4(self):
+        pass
+
+    def test_richtextctrl5(self):
+        pass
+
+    def test_richtextctrl6(self):
+        pass
         
 #---------------------------------------------------------------------------
 
