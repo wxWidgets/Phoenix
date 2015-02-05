1032875e26f275209c67d5e0a56ccffbb6c30a45
Werner F Bruhin <wernerfbd@gmx.ch>
2015-02-05 14:03:12 +0100

    - fix #!/usr/bin/env python

--- docs/sphinx/_downloads/i18nwxapp/app_base.py
+++ docs/sphinx/_downloads/i18nwxapp/app_base.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
 #
 # Author:   Werner F. Bruhin
 # Purpose:  how to I18N enable an application
--- docs/sphinx/_downloads/i18nwxapp/app_const.py
+++ docs/sphinx/_downloads/i18nwxapp/app_const.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
 #
 # Author:  Werner F. Bruhin
 # Purpose: Application constants
