87b92b66f0b95c0aff7c3c922e2e09860ee12c4a
Robin Dunn <robin@alldunn.com>
2015-02-05 16:52:18 -0800

    Remove unnecessary #! lines from files that will never be executed standalone.

--- sphinxtools/constants.py
+++ sphinxtools/constants.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
 
 #---------------------------------------------------------------------------
 # Name:        sphinxtools/constants.py
--- sphinxtools/templates.py
+++ sphinxtools/templates.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
 
 #---------------------------------------------------------------------------
 # Name:        sphinxtools/templates.py
