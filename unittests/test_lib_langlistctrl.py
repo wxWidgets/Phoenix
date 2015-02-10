5ea25818e91d8c5944be857bf9d4f58dcb619bcd
Werner F Bruhin <wernerfbd@gmx.ch>
2015-02-10 14:50:23 +0100

    - a basic test

---
+++ unittests/test_lib_langlistctrl.py
@@ -0,0 +1,21 @@
+import imp_unittest, unittest
+import wtc
+import wx
+import wx.lib.langlistctrl as lc
+
+#---------------------------------------------------------------------------
+
+class langlistctrl_Tests(wtc.WidgetTestCase):
+
+    def test_langlistctrlCtor(self):
+        theList = lc.LanguageListCtrl(self.frame, filter=lc.LC_ONLY, 
+                                      only=(wx.LANGUAGE_ENGLISH, 
+                                            wx.LANGUAGE_FRENCH), 
+                                      select=wx.LANGUAGE_ENGLISH)
+
+       
+#---------------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    unittest.main()
