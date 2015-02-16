35d9698453acd7f21f08b844952698ecec202869
Robin Dunn <robin@alldunn.com>
2015-02-16 13:32:33 -0800

    Add tests for richtextstyles module

--- unittests/test_richtextstyles.py
+++ unittests/test_richtextstyles.py
@@ -1,15 +1,68 @@
 import imp_unittest, unittest
 import wtc
 import wx
+import wx.richtext
 
 #---------------------------------------------------------------------------
 
 class richtextstyles_Tests(wtc.WidgetTestCase):
 
-    # TODO: Remove this test and add real ones.
-    def test_richtextstyles1(self):
-        self.fail("Unit tests for richtextstyles not implemented yet.")
+    def test_richtextstyles01(self):
+        ctrl = wx.richtext.RichTextStyleListCtrl(self.frame)
+
+
+    def test_richtextstyles02(self):
+        ctrl = wx.richtext.RichTextStyleListCtrl()
+        ctrl.Create(self.frame)
+
+
+    @unittest.expectedFailure   # RichTextStyleDefinition is abstract
+    def test_richtextstyles03(self):
+        sdef = wx.richtext.RichTextStyleDefinition()
         
+
+    def test_richtextstyles04(self):
+        sdef = wx.richtext.RichTextParagraphStyleDefinition()
+
+
+    def test_richtextstyles05(self):
+        ctrl = wx.richtext.RichTextStyleListBox(self.frame)
+
+
+    def test_richtextstyles06(self):
+        ctrl = wx.richtext.RichTextStyleListBox()
+        ctrl.Create(self.frame)
+
+
+    def test_richtextstyles05(self):
+        wx.richtext.RichTextStyleListBox.RICHTEXT_STYLE_ALL
+        wx.richtext.RichTextStyleListBox.RICHTEXT_STYLE_PARAGRAPH
+        wx.richtext.RichTextStyleListBox.RICHTEXT_STYLE_CHARACTER
+        wx.richtext.RichTextStyleListBox.RICHTEXT_STYLE_LIST
+        wx.richtext.RichTextStyleListBox.RICHTEXT_STYLE_BOX
+        
+
+    def test_richtextstyles6(self):
+        ctrl = wx.richtext.RichTextStyleComboCtrl(self.frame)
+
+
+    def test_richtextstyles07(self):
+        ctrl = wx.richtext.RichTextStyleComboCtrl()
+        ctrl.Create(self.frame)
+
+
+    def test_richtextstyles08(self):
+        sdef = wx.richtext.RichTextCharacterStyleDefinition()
+
+
+    def test_richtextstyles09(self):
+        sdef = wx.richtext.RichTextListStyleDefinition()
+
+
+    def test_richtextstyles10(self):
+        sdef = wx.richtext.RichTextStyleSheet()
+
+    
 #---------------------------------------------------------------------------
 
 if __name__ == '__main__':
