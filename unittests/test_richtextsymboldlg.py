66631aa0b3e76a23d5a211923ab2e589eb49f0b0
Robin Dunn <robin@alldunn.com>
2015-02-16 15:54:18 -0800

    Add tests for the richtextsymboldlg module

--- unittests/test_richtextsymboldlg.py
+++ unittests/test_richtextsymboldlg.py
@@ -1,15 +1,25 @@
 import imp_unittest, unittest
 import wtc
 import wx
+import wx.richtext
 
 #---------------------------------------------------------------------------
 
 class richtextsymboldlg_Tests(wtc.WidgetTestCase):
 
-    # TODO: Remove this test and add real ones.
     def test_richtextsymboldlg1(self):
-        self.fail("Unit tests for richtextsymboldlg not implemented yet.")
+        dlg = wx.richtext.SymbolPickerDialog('', '', '', self.frame)
+        dlg.Show()
+        dlg.Destroy()
         
+
+    def test_richtextsymboldlg2(self):
+        dlg = wx.richtext.SymbolPickerDialog()
+        dlg.Create('', '', '', self.frame)
+        dlg.Show()
+        dlg.Destroy()
+
+
 #---------------------------------------------------------------------------
 
 if __name__ == '__main__':
