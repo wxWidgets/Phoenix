464f3c5c399e9f0b18bff05130162462f3b00f10
Robin Dunn <robin@alldunn.com>
2015-02-16 16:00:23 -0800

    Add test for richtextxml

--- unittests/test_richtextxml.py
+++ unittests/test_richtextxml.py
@@ -1,14 +1,16 @@
 import imp_unittest, unittest
 import wtc
 import wx
+import wx.richtext
 
 #---------------------------------------------------------------------------
 
 class richtextxml_Tests(wtc.WidgetTestCase):
 
-    # TODO: Remove this test and add real ones.
     def test_richtextxml1(self):
-        self.fail("Unit tests for richtextxml not implemented yet.")
+        handler = wx.richtext.RichTextXMLHandler()
+        wx.richtext.RichTextBuffer.AddHandler(handler)
+
         
 #---------------------------------------------------------------------------
 
