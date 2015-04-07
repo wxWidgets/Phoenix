32b5d578fae3b0d52f02e6d2e5ac69183e18cafd
wernerfb <wernerfbd@gmx.ch>
2015-04-07 17:41:10 +0200

    add a simple test

---
+++ unittests/test_lib_resizewidget.py
@@ -0,0 +1,26 @@
+import imp_unittest, unittest
+import wtc
+import wx
+import wx.lib.resizewidget as RW
+
+#---------------------------------------------------------------------------
+
+class lib_resizewidget_Tests(wtc.WidgetTestCase):
+
+    def test_lib_resizewidget(self):
+        rw1 = RW.ResizeWidget(self.frame)
+        
+    def test_lib_resizewidget_Constants(self):
+        RW.RW_FILL
+        RW.RW_FILL2
+        RW.RW_LENGTH
+        RW.RW_PEN
+        RW.RW_THICKNESS
+        
+        RW.EVT_RW_LAYOUT_NEEDED
+        
+        
+#---------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    unittest.main()
