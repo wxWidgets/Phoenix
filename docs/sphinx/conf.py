31dcd1f021426269b2d5bc4c042955ad419bb0ba
Robin Dunn <robin@alldunn.com>
2016-05-18 16:28:29 -0700

    Update copyright

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'wxPython (Phoenix)'
-copyright = u'2012-2014, The wxPython Team'
+copyright = u'2012-2016, The wxPython Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
