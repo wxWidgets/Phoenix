2fd98b724bc3711adfcccd7426bad5ed7df9c2c5
Douglas Thor <doug.thor@gmail.com>
2016-07-05 13:41:08 -0700

    Moved changelog and readme to files.

---
+++ wx/lib/plot/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+wx.lib.plot
+===========
+
+This is a simple plotting library for the wxPython Phoenix project.
+
+"""
+__version__ = "0.0.1"
+__updated__ = "2016-07-05"
+
+
+# Expose items to the old API
+# Old: import wx.lib.plot as wxplot
+# New: from wx.lib.plot import plot as wxplot
+
+
+
+
+__all__ = []
