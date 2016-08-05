a672913e97c776bcf051290ba194a03ff51f409e
Robin Dunn <robin@alldunn.com>
2016-08-05 12:08:21 -0700

    Refactor custom Create code to a separate helper function. Add a replacement for the constructor taking arrays of rgb values. Update and expand tests.

--- etg/palette.py
+++ etg/palette.py
@@ -30,7 +30,87 @@ def run():
     # Tweak the parsed meta objects in the module object as needed for
     # customizing the generated code and docstrings.
     c = module.find('wxPalette')
+    tools.removeVirtuals(c)
 
+
+    # Add a helper function for use with the Create method and the Ctor
+    # accepting RGB data.
+    c.addCppCode("""\
+        // a helper function to be used in the Create method and one of the Ctors
+        bool _paletteCreateHelper(wxPalette* self,
+                                  PyObject* red, PyObject* green, PyObject* blue) {
+            bool rval = false;
+            wxPyThreadBlocker blocker;
+            char* errMsg = "Expected a sequence of integer objects";
+
+            if (!PySequence_Check(red) || !PySequence_Check(green) || !PySequence_Check(blue)) {
+                PyErr_SetString(PyExc_TypeError, errMsg);
+                return rval;
+            }
+
+            Py_ssize_t count = PySequence_Size(red);
+            if (PySequence_Size(green) != count || PySequence_Size(blue) != count) {
+                PyErr_SetString(PyExc_ValueError, "Sequence lengths must be equal");
+                return rval;
+            }
+
+            unsigned char* redArray = new unsigned char[count];
+            unsigned char* greenArray = new unsigned char[count];
+            unsigned char* blueArray = new unsigned char[count];
+
+            for (Py_ssize_t i = 0; i < count; i++) {
+                PyObject* redItem = PySequence_ITEM(red, i);
+                PyObject* greenItem = PySequence_ITEM(green, i);
+                PyObject* blueItem = PySequence_ITEM(blue, i);
+                if (!wxPyInt_Check(redItem) || !wxPyInt_Check(greenItem) || !wxPyInt_Check(blueItem)) {
+                    PyErr_SetString(PyExc_TypeError, errMsg);
+                    goto pch_exit;
+                }
+
+                long redLong = wxPyInt_AsLong(redItem);
+                long greenLong = wxPyInt_AsLong(greenItem);
+                long blueLong = wxPyInt_AsLong(blueItem);
+                Py_DECREF(redItem);
+                Py_DECREF(greenItem);
+                Py_DECREF(blueItem);
+                if (redLong < 0 || redLong > 255 || greenLong < 0 || greenLong > 255 || blueLong < 0 || blueLong > 255) {
+                    PyErr_SetString(PyExc_ValueError, "Sequence values must be in the 0..255 range");
+                    goto pch_exit;
+                }
+                redArray[i] = redLong;
+                greenArray[i] = greenLong;
+                blueArray[i] = blueLong;
+            }
+
+            rval = self->Create(count, redArray, greenArray, blueArray);
+
+        pch_exit:
+            delete[] redArray;
+            delete[] greenArray;
+            delete[] blueArray;
+            return rval;
+        }
+        """)
+
+
+    #-----------------------------------------------------------------
+
+    # Replace the constructor accepting the arrays of RGB values with one that
+    # understands any Python sequence.
+    c.find('wxPalette').findOverload('red').ignore()
+    c.addCppCtor('(PyObject* red, PyObject* green, PyObject* blue)',
+        doc="""\
+            Creates a palette from a set of sequences of integers,
+            one for each red, green and blue color components.""",
+        body="""\
+            wxPalette* pal = new wxPalette;
+            _paletteCreateHelper(pal, red, green, blue);
+            if (PyErr_Occurred()) {
+                delete pal;
+                return NULL;
+            }
+            return pal;
+            """)
 
     c.find('GetPixel.red').pyInt = True
     c.find('GetPixel.green').pyInt = True
@@ -55,68 +135,19 @@ def run():
             return rv;
             """)
 
+    # Replace the Create method with one that understands any kind of Python sequence
     c.find('Create').ignore()
     c.addCppMethod('PyObject*', 'Create', '(PyObject* red, PyObject* green, PyObject* blue)',
         doc="Creates a palette from 3 sequences of integers, one for each red, blue or green component.",
         body="""\
+            bool rval = _paletteCreateHelper(self, red, green, blue);
             wxPyThreadBlocker blocker;
-            PyObject* rval;
-
-            char* errMsg = "Expected a sequence of integer objects";
-            if (!PySequence_Check(red) || !PySequence_Check(green) || !PySequence_Check(blue)) {
-                PyErr_SetString(PyExc_TypeError, errMsg);
+            if (PyErr_Occurred())
                 return NULL;
-            }
-
-            Py_ssize_t count = PySequence_Size(red);
-            if (PySequence_Size(green) != count || PySequence_Size(blue) != count) {
-                PyErr_SetString(PyExc_ValueError, "Sequence lengths must be equal");
-                return NULL;
-            }
-
-            unsigned char* redArray = new unsigned char[count];
-            unsigned char* greenArray = new unsigned char[count];
-            unsigned char* blueArray = new unsigned char[count];
-
-            for (Py_ssize_t i = 0; i < count; i++) {
-                PyObject* redItem = PySequence_ITEM(red, i);
-                PyObject* greenItem = PySequence_ITEM(green, i);
-                PyObject* blueItem = PySequence_ITEM(blue, i);
-                if (!wxPyInt_Check(redItem) || !wxPyInt_Check(greenItem) || !wxPyInt_Check(blueItem)) {
-                    PyErr_SetString(PyExc_TypeError, errMsg);
-                    rval = NULL;
-                    goto exit;
-                }
-
-                long redLong = wxPyInt_AsLong(redItem);
-                long greenLong = wxPyInt_AsLong(greenItem);
-                long blueLong = wxPyInt_AsLong(blueItem);
-                Py_DECREF(redItem);
-                Py_DECREF(greenItem);
-                Py_DECREF(blueItem);
-                if (redLong < 0 || redLong > 255 || greenLong < 0 || greenLong > 255 || blueLong < 0 || blueLong > 255) {
-                    PyErr_SetString(PyExc_ValueError, "Sequence values must be >= 0 and < 256");
-                    rval = NULL;
-                    goto exit;
-                }
-                redArray[i] = redLong;
-                greenArray[i] = greenLong;
-                blueArray[i] = blueLong;
-            }
-
-            if (self->Create(count, redArray, greenArray, blueArray)) {
-                rval = Py_True;
-                Py_INCREF(rval);
-            } else {
-                rval = Py_False;
-                Py_INCREF(rval);
-            }
-
-            exit:
-                delete[] redArray;
-                delete[] greenArray;
-                delete[] blueArray;
-                return rval;
+            if (rval)
+                Py_RETURN_TRUE;
+            else
+                Py_RETURN_FALSE;
             """)
 
 
