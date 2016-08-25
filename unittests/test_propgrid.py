85db11e72c5c37ee7b47389cbefb7a7808815a29
Robin Dunn <robin@alldunn.com>
2016-08-25 14:20:09 -0700

    Add simple unittests for items in the propgridiface etg module

--- unittests/test_propgrid.py
+++ unittests/test_propgrid.py
@@ -7,11 +7,11 @@ import wx.propgrid as pg
 
 class propgrid_Tests(wtc.WidgetTestCase):
 
-    def test_propgrid1(self):
+    def test_propgrid01(self):
         obj = pg.PGValidationInfo()
 
 
-    def test_propgrid0(self):
+    def test_propgrid02(self):
         pg.PG_AUTO_SORT
         pg.PG_HIDE_CATEGORIES
         pg.PG_ALPHABETIC_MODE
@@ -25,6 +25,7 @@ class propgrid_Tests(wtc.WidgetTestCase):
         pg.PG_TOOLBAR
         pg.PG_DESCRIPTION
         pg.PG_NO_INTERNAL_BORDER
+
         pg.PG_EX_INIT_NOCAT
         pg.PG_EX_NO_FLAT_TOOLBAR
         pg.PG_EX_MODE_BUTTONS
@@ -37,6 +38,7 @@ class propgrid_Tests(wtc.WidgetTestCase):
         pg.PG_EX_ENABLE_TLP_TRACKING
         pg.PG_EX_NO_TOOLBAR_DIVIDER
         pg.PG_EX_TOOLBAR_SEPARATOR
+
         pg.PG_VFB_STAY_IN_PROPERTY
         pg.PG_VFB_BEEP
         pg.PG_VFB_MARK_CELL
@@ -44,6 +46,7 @@ class propgrid_Tests(wtc.WidgetTestCase):
         pg.PG_VFB_SHOW_MESSAGEBOX
         pg.PG_VFB_SHOW_MESSAGE_ON_STATUSBAR
         pg.PG_VFB_DEFAULT
+
         pg.PG_ACTION_INVALID
         pg.PG_ACTION_NEXT_PROPERTY
         pg.PG_ACTION_PREV_PROPERTY
@@ -53,10 +56,18 @@ class propgrid_Tests(wtc.WidgetTestCase):
         pg.PG_ACTION_EDIT
         pg.PG_ACTION_PRESS_BUTTON
         pg.PG_ACTION_MAX
-        
+
         pg.PG_DEFAULT_STYLE
         pg.PGMAN_DEFAULT_STYLE
-        
+
+
+    def test_propgrid03(self):
+        pgrid = pg.PropertyGrid(self.frame)
+
+
+
+
+
 #---------------------------------------------------------------------------
 
 if __name__ == '__main__':
--- unittests/test_propgridiface.py
+++ unittests/test_propgridiface.py
@@ -7,10 +7,17 @@ import wx.propgrid as pg
 
 class propgridiface_Tests(wtc.WidgetTestCase):
 
-    # TODO: Remove this test and add real ones.
-    def test_propgridiface1(self):
-        self.fail("Unit tests for propgridiface not implemented yet.")
-        
+    def test_propgridiface01(self):
+        arg = pg.PGPropArgCls('test_arg')
+
+
+    def test_propgridiface02(self):
+        with self.assertRaises(TypeError):
+            iface = pg.PropertyGridInterface()
+
+
+
+
 #---------------------------------------------------------------------------
 
 if __name__ == '__main__':
