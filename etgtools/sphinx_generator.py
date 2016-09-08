1a94ced4b3bda82f1efed06fdb42332e926446dc
Robin Dunn <robin@alldunn.com>
2016-09-08 12:32:11 -0700

    a bit of cleanup

--- etgtools/sphinx_generator.py
+++ etgtools/sphinx_generator.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
-
 #---------------------------------------------------------------------------
 # Name:        etgtools/sphinx_generator.py
 # Author:      Andrea Gavana
