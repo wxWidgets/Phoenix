fc85531951be402009072552053033837fa7463a
Robin Dunn <robin@alldunn.com>
2016-09-08 12:29:28 -0700

    first pass over propgrid code snippets

---
+++ docs/sphinx/rest_substitutions/snippets/python/converted/wx.propgrid.DirProperty.DoGetValidator.1.py
@@ -0,0 +1,9 @@
+    
+    class MyPropertyClass(wx.propgrid.DirProperty):
+        ...
+        def DoGetValidator(self):
+            validator = MyValidator(...)
+    
+            ... prepare validator...
+    
+            return validator
