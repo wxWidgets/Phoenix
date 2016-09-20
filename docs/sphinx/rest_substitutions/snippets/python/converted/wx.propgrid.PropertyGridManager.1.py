ad4196b7a71bb7322147591bb600d1dd5b99a6b6
Robin Dunn <robin@alldunn.com>
2016-09-19 17:27:57 -0700

    the rest of the propgrid doc snippets

---
+++ docs/sphinx/rest_substitutions/snippets/python/converted/wx.propgrid.PropertyGridManager.1.py
@@ -0,0 +1,28 @@
+    
+    pgMan = wx.propgrid.PropertyGridManager(
+        parent,
+        PGID,
+        # These and other similar styles are automatically
+        # passed to the embedded wx.PropertyGrid.
+        style = wx.PG_BOLD_MODIFIED|wx.PG_SPLITTER_AUTO_CENTER|
+        # Include toolbar.
+        wx.PG_TOOLBAR |
+        # Include description box.
+        wx.PG_DESCRIPTION |
+        # Include compactor.
+        wx.PG_COMPACTOR |
+        # Plus defaults.
+        wx.PGMAN_DEFAULT_STYLE
+      )
+    
+    page = pgMan.AddPage("First Page")
+    page.Append(wx.propgrid.PropertyCategory("Category A1"))
+    page.Append(wx.propgrid.IntProperty("Number", wx.propgrid.PG_LABEL, 1))
+    page.Append(wx.propgrid.ColourProperty("Colour",wx.propgrid.PG_LABEL, wx.WHITE))
+    
+    page = pgMan.AddPage("Second Page")
+    page.Append("Text", wx.propgrid.PG_LABEL, "(no text)")
+    page.Append(wx.propgrid.FontProperty("Font",wx.propgrid.PG_LABEL))
+    
+    # Display a header above the grid
+    pgMan.ShowHeader()
