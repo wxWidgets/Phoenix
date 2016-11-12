46eef97a193747e266454a8f769de3045a7fd5e6
Robin Dunn <robin@alldunn.com>
2016-11-11 19:39:18 -0800

    Add some unittests for AUI

--- unittests/test_auitabmdi.py
+++ unittests/test_auitabmdi.py
@@ -1,15 +1,23 @@
 import unittest
 from unittests import wtc
 import wx
+import wx.aui
 
 #---------------------------------------------------------------------------
 
 class auitabmdi_Tests(wtc.WidgetTestCase):
 
-    # TODO: Remove this test and add real ones.
-    def test_auitabmdi1(self):
-        self.fail("Unit tests for auitabmdi not implemented yet.")
-        
+    def test_auitabmdi01(self):
+        parent = wx.aui.AuiMDIParentFrame(self.frame, title='AUI MDI')
+        child = wx.aui.AuiMDIChildFrame(parent, title='Child')
+
+        parent.Show()
+        self.myYield()
+        child.Close()
+        self.myYield()
+        parent.Close()
+
+
 #---------------------------------------------------------------------------
 
 if __name__ == '__main__':
