51ed2517c7b336f4a6d86df7f2ed1e6fd6d336a3
Robin Dunn <robin@alldunn.com>
2016-11-23 21:44:34 -0800

    Add deprecated PyWizardPage

--- etg/wizard.py
+++ etg/wizard.py
@@ -3,54 +3,55 @@
 # Author:      Robin Dunn
 #
 # Created:     06-Jun-2012
-# Copyright:   (c) 2013 by Total Control Software
+# Copyright:   (c) 2012 - 2016 by Total Control Software
 # License:     wxWindows License
 #---------------------------------------------------------------------------
 
 import etgtools
 import etgtools.tweaker_tools as tools
 
-PACKAGE   = "wx"   
+PACKAGE   = "wx"
 MODULE    = "_adv"
 NAME      = "wizard"   # Base name of the file to generate to for this script
 DOCSTRING = ""
 
 # The classes and/or the basename of the Doxygen XML files to be processed by
-# this script. 
+# this script.
 ITEMS  = [ "wxWizardPage",
            "wxWizardPageSimple",
            "wxWizard",
            "wxWizardEvent",
-           ]    
-    
+           ]
+
 #---------------------------------------------------------------------------
 
 def run():
     # Parse the XML file(s) building a collection of Extractor objects
     module = etgtools.ModuleDef(PACKAGE, MODULE, NAME, DOCSTRING)
     etgtools.parseDoxyXML(module, ITEMS)
-    
+
     #-----------------------------------------------------------------
     # Tweak the parsed meta objects in the module object as needed for
     # customizing the generated code and docstrings.
-    
+
     c = module.find('wxWizardPage')
     assert isinstance(c, etgtools.ClassDef)
     tools.fixWindowClass(c, False)
-    
-    
+    module.addPyCode("PyWizardPage = wx.deprecated(WizardPage, 'Use WizardPage instead.')")
+
+
     c = module.find('wxWizardPageSimple')
     tools.fixWindowClass(c, False)
     c.addItem(etgtools.WigCode("""\
         virtual wxWizardPage* GetNext() const;
         virtual wxWizardPage* GetPrev() const;
         """))
-    
+
 
     c = module.find('wxWizard')
     tools.fixWindowClass(c, False)
-    
-    
+
+
     c = module.find('wxWizardEvent')
     tools.fixEventClass(c)
     c.addPyCode("""\
@@ -62,13 +63,13 @@ def run():
         EVT_WIZARD_FINISHED      = wx.PyEventBinder( wxEVT_WIZARD_FINISHED, 1)
         EVT_WIZARD_PAGE_SHOWN    = wx.PyEventBinder( wxEVT_WIZARD_PAGE_SHOWN, 1)    
         """)
-    
-    
+
+
     #-----------------------------------------------------------------
     tools.doCommonTweaks(module)
     tools.runGenerators(module)
-    
-    
+
+
 #---------------------------------------------------------------------------
 if __name__ == '__main__':
     run()
