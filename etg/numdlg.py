98135c2220f948e16b63c7b18fbfe4e641ec9818
Robin Dunn <robin@alldunn.com>
2017-01-21 20:13:06 -0800

    Add numdlg

---
+++ etg/numdlg.py
@@ -0,0 +1,49 @@
+#---------------------------------------------------------------------------
+# Name:        etg/numdlg.py
+# Author:      Robin Dunn
+#
+# Created:     21-Jan-2017
+# Copyright:   (c) 2017 by Total Control Software
+# License:     wxWindows License
+#---------------------------------------------------------------------------
+
+import etgtools
+import etgtools.tweaker_tools as tools
+
+PACKAGE   = "wx"
+MODULE    = "_core"
+NAME      = "numdlg"   # Base name of the file to generate to for this script
+DOCSTRING = ""
+
+# The classes and/or the basename of the Doxygen XML files to be processed by
+# this script.
+ITEMS  = [ 'numdlg_8h.xml',
+
+           ]
+
+#---------------------------------------------------------------------------
+
+def run():
+    # Parse the XML file(s) building a collection of Extractor objects
+    module = etgtools.ModuleDef(PACKAGE, MODULE, NAME, DOCSTRING)
+    etgtools.parseDoxyXML(module, ITEMS)
+
+    #-----------------------------------------------------------------
+    # Tweak the parsed meta objects in the module object as needed for
+    # customizing the generated code and docstrings.
+
+    module.addHeaderCode('#include <wx/numdlg.h>')
+
+    c = module.find('wxGetNumberFromUser')
+    c.mustHaveApp()
+
+
+    #-----------------------------------------------------------------
+    tools.doCommonTweaks(module)
+    tools.runGenerators(module)
+
+
+#---------------------------------------------------------------------------
+if __name__ == '__main__':
+    run()
+
