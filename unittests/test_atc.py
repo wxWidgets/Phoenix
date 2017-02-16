44436070b8e68136c18757af41fb48f1afce0fb7
Samuel Dunn <samuel@mesalu.com>
2017-02-16 13:34:28 -0800

    Added additional test case to test_atc, renamed test class name in test_frame

--- unittests/test_atc.py
+++ unittests/test_atc.py
@@ -23,10 +23,26 @@ class ATCPanel(wx.Panel, atc.TestWidget):
 
     @unittest.skip("reasons")
     def test_skip(self):
+        self.testFailed("test_skip was invoked!")
+
+    @unittest.skip("Takes too long for regular testing")
+    def test_watchdog(self):
+        print("Letting application loose")
         return
 
+class ATCFrame(wx.Frame, atc.TestWidget):
+    def __init__(self):
+        wx.Frame.__init__(self, None, wx.NewId(), "ATC Test Frame")
+        atc.TestWidget.__init__(self)
 
-testcase = atc.createATC(ATCPanel)
+    def test_pass(self):
+        self.testPassed()
+
+    @unittest.expectedFailure
+    def test_fail(self):
+        self.testFailed("Deliberate test failure")
+
+atc_BasicTests = atc.createATC(ATCPanel)
 
 if __name__ == "__main__":
     unittest.main()
