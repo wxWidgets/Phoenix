c4ce20761d768909ea4c653324f3008ccad0aafc
Samuel Dunn <samuel@mesalu.com>
2017-02-16 13:32:10 -0800

    Added a ton of documentation. Rearranged some code to put user-exposed methods and classes first added dunders to methods not intended to be user-accessible.

--- unittests/atc.py
+++ unittests/atc.py
@@ -62,91 +62,233 @@ def testCritical(func):
 
     return method
 
+def createATC(widget):
+    """
+    Creates and returns a class that derives unittest.TestCase the TestCase is generated from widget
+    IMPORTANT NOTE: In order for the returned class to be picked up by the default unittest TestDiscovery process
+        the returned class must be assigned to the main module's base level namespace
+    If widget is not top level  (does not drive from wx.Frame) a container top level widget will be created
+        Additionally, if the widget is not top level generated frame object will attempt to pass
+            itself as a parameter to widget.__init__ (for assigning parent) 
+            Be sure to expect this in such scenarios.
+
+    The returned class will have test_ methods to match those of the widget class, all decorations are preserved
+        so it is perfectly valid to apply unittest decorators (such as unittest.expectedFailure) to the widget class
+        as such decorations will be reflected by the TestCase class methods and utilized during testing.
+
+    Args:
+        widget: A widget *class* that derives from TestWidget.
+
+    Returns:
+        unittest.TestCase derivation
+
+    Example:
+    class Foo(wx.Frame, atc.TestWidget):
+        def __init__(self):
+            wx.Frame.__init__(self, None, wx.NewId())
+            atc.TestWidget.__init__(self)
+
+        def test_pass(self):
+            self.testPassed()
+
+        @unittest.expectedFailure
+        def test_fail(self):
+            self.testFailed("Deliberate failure")
+
+    FooTestCase = atc.createATC(Foo)
+
+    # FooTestCase will have a methods test_pass and test_fail
+    # when run by pytest test_pass will pass and test_fail will
+    # xfail.
+    """
+    assert issubclass(widget, TestWidget), "Testing requires the tested widget to derive from TestWidget for now"
+
+    tlw = None
+    if not issubclass(widget, wx.Frame):
+        # need to stick this widget in a frame
+        tlw = __CreateFrame(widget)
+    else:
+        tlw = widget
+
+    app = __CreateApp(tlw)
+
+    class ApplicationTestCase(unittest.TestCase):
+        pass
+
+    methods = [meth for meth in dir(widget) if (meth.startswith("test_") and callable(getattr(widget, meth)))]
+
+    for meth in methods:
+        test_func = __CreateTestMethod(app, meth)
+        # ensure any other deocrated data is preserved:
+        basemeth = getattr(widget, meth)
+        for attr in dir(basemeth):
+            if not hasattr(test_func, attr):
+                setattr(test_func, attr, getattr(basemeth, attr))
+
+        setattr(ApplicationTestCase, meth, test_func)
+
+    return ApplicationTestCase
 
 class TestWidget:
+    """
+    Base Test Widget class. Widgets that are intended to be tested via ATC *MUST* derive this class
+    It is likewise expected that these widgets derive from some actual wx widget, at the minimum from wx.EvtHandler,  the constructor for which must be called first
+    This class provides test sequencing to its derived class, most notably in starting the test automatically, and providing methods for termination.
+    For example usage please review unittests/test_atc.
+    """
     def __init__(self):
+        """
+        Ensures that class being instantiated also derives from wx.EvtHandler and prepares for auto-launch
+        Args: 
+            self
+        """
         assert isinstance(self, wx.EvtHandler), "Test widget needs to be an event handler"
 
-        self.Bind(EVT_TEST, self.OnTest)
+        self.Bind(EVT_TEST, self.__OnTest)
 
         if "__WXGTK__" in wx.PlatformInfo:
-            self.Bind(wx.EVT_WINDOW_CREATE, self.OnLnxStart)
+            self.Bind(wx.EVT_WINDOW_CREATE, self.__OnLnxStart)
 
         else:
-            wx.CallAfter(self.OnCommence)
+            wx.CallAfter(self.__OnCommence)
+    
+    def testPassed(self):
+        """
+        Indicates that the currently running test has passed successfully.
+        The application will close clearly after this call, allowing  unittesting to proceed
+        IMPORTANT NOTE: test sequences must terminate with either a call to this method or testFailed()
+            otherwise runtime may not close until the watchdog triggers (treating the test as a failure)
+        
+        Args:
+            None
 
-    def OnCommence(self):
-        assert wx.GetApp().IsMainLoopRunning(), "Timer ended before mainloop started" # see above comment regarding
-                                                                                    # commencing with the timer.
+        Returns:
+            None
+        """
+        for window in wx.GetTopLevelWindows():
+            window.Close()
+
+    def testFailed(self, errmsg =  "A test failed."):
+        """
+        Indicates that the currently running test has failed. 
+        The application will close after this call and an TestError exception will be raised
+        IMPORTANT NOTE: test sequences must terminate with either a call to this method or testPassed()
+            otherwise runtime may not close until the watchdog triggers (treating the test as a failure)
+
+        Args: 
+            errmsg: Message assigned to TestError exception.
+        Returns:
+            None
+        """
+
+        # do not rely on testCritical being applied to an above method
+        wx.GetApp().exception = TestError(errmsg)
+        for window in wx.GetTopLevelWindows():
+            window.Close()
+    
+    def __OnCommence(self):
+        """
+        Invoked to start test procedures. Invokation is handled by atc.
+        """
+        # assert should exit properly if the mainloop is  not running
+        assert wx.GetApp().IsMainLoopRunning(), "__OnCommence invoked before MainLoop was ready"
+
         # start test sequence
         evt = TestEvent()
         evt.case = wx.GetApp().case
         wx.PostEvent(self, evt)
 
         # set a watchdog incase of test error
-        wx.CallLater(300000, self.OnWatchdog) # 5 minutes in millis
+        wx.CallLater(300000, self.__OnWatchdog) # 5 minutes in millis
 
-    def OnWatchdog(self):
+    def __OnWatchdog(self):
+        """ 
+        Invoked with a test hass taken more than 5 minutes to complete
+        """
         print("Test Timeout!!!")
         wx.GetApp().exception = RuntimeError("Watchdog timed out")
         for window in wx.GetTopLevelWindows():
             window.Close()
 
-    def OnLnxStart(self, evt):
+    def __OnLnxStart(self, evt):
         """
         Invoked on linux systems to signal mainloop readiness
         """
-        wx.CallAfter(self.OnCommence)
+        wx.CallAfter(self.__OnCommence)
         evt.Skip()
 
     @testCritical   # automatically apply exception blocking to test_ methods.. indirectly
-    def OnTest(self, evt):
+    def __OnTest(self, evt):
         testfunc = getattr(self, evt.case)
 
         print("Testing: %s" % evt.case)
         testfunc()
-
-    def testPassed(self):
-        for window in wx.GetTopLevelWindows():
-            window.Close()
-
-    def testFailed(self, errmsg =  "A test failed."):
-        # do not rely on testCritical being applied to an above method
-        wx.GetApp().exception = TestError(errmsg)
-        for window in wx.GetTopLevelWindows():
-            window.Close()
         
-def CreateApp(frame):
+def __CreateApp(frame_cls):
+    """
+    Generates an app class that will create an instance of frame_cls on launch.
+    This method is utilized inside atc and probably should not be used otherwise
+
+    Args:
+        frame_cls: Class that derives from wx.Frame
+    Returns:
+        wx.App derived class
+    """
     class TestApp(wx.App):
+        """ a generated App class """
         def OnInit(self):
-            self.frame = frame()
+            self.frame = frame_cls()
             self.frame.Show(True)
             return True
 
     return TestApp
 
-def CreateFrame(widget):
+def __CreateFrame(widget_cls):
+    """
+    Creates a wx.Frame derivation to create an instance of widget_cls and sizes said instance to fill the frame
+    This method is utlized inside atc and probably should not be used otherwise
+    
+    Args:
+        widget_cls: Widget class (presumably the TestWidget) that will be initialized within the generated frame object
+
+    Returns:
+        wx.Frame derived class to use for testing the widget.
+    """
     # called when the test widget is not a frame.
     class BaseTestFrame(wx.Frame):
+        """ A generated Frame class """
         def __init__(self):
-            wx.Frame.__init__(self, None, wx.NewId(), "%s Test Frame" % str(type(widget)))
+            wx.Frame.__init__(self, None, wx.NewId(), "Generated Test Frame")
 
             sizer = wx.BoxSizer()
-            self.widget = widget(self)  # assumes need of parent.
+            self.widget = widget_cls(self)  # assumes need of parent.
             sizer.Add(self.widget, 1, wx.EXPAND)
 
             self.SetSizer(sizer)
             sizer.Layout()
 
-        # this will only be invoked if the test widget is not a frame.
-        def GetTestTarget(self):
-            return self.widget
-
     return BaseTestFrame
 
-def CreateTestMethod(app, case):
+def __CreateTestMethod(app_cls, case):
+    """
+    Generates the test methods to assign to the ApplicationTestCase class.
+    Generated methods initialize an instance of app_cls, assign the intended test case and 
+        launch the app.
+    Once the app has closed error conditions are checked and  the function returns
+
+    This method is utilized inside atc and should not be used otherwise.
+
+    returned method objects are not redecorated (as critical class data is not present)
+
+    Args:
+        app_cls: the wx.App derived class to instantiate
+        case: str indicating which  test method to retrieve and  invoke.
+    
+    Returns:
+        Method objects to launch an application for a given test case.
+    """
     def test_func(obj):
-        a = app()
+        a = app_cls()
         a.case = case
         a.MainLoop()
 
@@ -157,35 +299,3 @@ def CreateTestMethod(app, case):
             sys.exit(a.errorcode)
 
     return test_func
-
-def createATC(widget):
-    # if widget is not instance of TestFrame, generate a quick frame
-    # to house the widget
-    assert issubclass(widget, TestWidget), "Testing requires the tested widget to derive from TestWidget for now"
-
-    tlw = None
-    if not issubclass(widget, wx.Frame):
-        # need to stick this widget in a frame
-        tlw = CreateFrame(widget)
-
-    else:
-        tlw = widget
-
-    app = CreateApp(tlw)
-
-    class ApplicationTestCase(unittest.TestCase):
-        pass
-
-    methods = [meth for meth in dir(widget) if (meth.startswith("test_") and callable(getattr(widget, meth)))]
-
-    for meth in methods:
-        test_func = CreateTestMethod(app, meth)
-        # ensure any other deocrated data is preserved:
-        basemeth = getattr(widget, meth)
-        for attr in dir(basemeth):
-            if not hasattr(test_func, attr):
-                setattr(test_func, attr, getattr(basemeth, attr))
-
-        setattr(ApplicationTestCase, meth, test_func)
-
-    return ApplicationTestCase
