853c9d4df82aec68b383c33b629d25026f935d10
James Wettenhall <James.Wettenhall@monash.edu>
2017-03-17 17:19:43 +1100

    wxTipWindow should be shown automatically after it's instantiated. Trying to Show it manually in this test seems to result in: E       wxAssertionError: C++ assertion "!wxMouseCapture::IsInCaptureStack(this) " failed at ..\..\src\common\wincmn.cpp(3271) in wxWindowBase::CaptureMouse(): R ecapturing the mouse in the same window?

--- unittests/test_tipwin.py
+++ unittests/test_tipwin.py
@@ -9,8 +9,10 @@ class tipwin_Tests(wtc.WidgetTestCase):
     def test_tipwinCtor(self):
         w = wx.TipWindow(self.frame, "This is a tip message")
         w.SetBoundingRect(self.frame.GetRect())
-        self.waitFor(100)
-        w.Show()
+        self.waitFor(200)
+        # wxTipWindow constructor docs say:
+        # "The tip is shown immediately after the window is constructed."
+        assert w.IsShown()
         self.waitFor(100)
         w.Close()
 
