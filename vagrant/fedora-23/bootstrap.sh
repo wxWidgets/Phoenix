8b0956fef51e4f8628363fb5860f33e427ad4ad2
Robin Dunn <robin@alldunn.com>
2017-09-12 14:59:05 -0700

    Cleanup the F23 bootstrap a little

--- vagrant/fedora-23/bootstrap.sh
+++ vagrant/fedora-23/bootstrap.sh
@@ -15,16 +15,16 @@ dnf -y install gtk2 gtk2-devel gtk3 gtk3-devel \
     SDL SDL-devel gstreamer gstreamer-devel gstreamer-plugins-base-devel \
     freeglut freeglut-devel libnotify libnotify-devel libSM-devel
 
-dnf -y install python35
 # Install all available Python packages and their dev packages
 dnf -y install python python-tools python-devel python-virtualenv
 dnf -y install python34 python34-tools python34-devel
 dnf -y install python3 python3-tools python3-devel
+dnf -y install python35
 
 # Set up virtual environments for each Python where the Phoenix builds will be
-# done. set them to the vagrant user so the venv's can be updated by pip later.
+# done. Set them to the vagrant user so the venv's can be updated by pip later.
 mkdir venvs
 virtualenv --python=python2.7 venvs/Py27
-pyvenv-3.4 venvs/Py34
-pyvenv-3.5 venvs/Py35
+python3.4 -m venv venvs/Py34
+python3.5 -m venv venvs/Py35
 chown -R vagrant:vagrant venvs
