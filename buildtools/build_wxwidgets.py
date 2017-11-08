7b58eb5c41c64c7c9d18d2f5ca6fd9aee1fd8481
kollivier <kevino@theolliviers.com>
2017-11-08 13:43:30 -0800

    Set the min OS X version to 10.6 as the SDK for 10.13+ need weak-linking features present only in 10.6 and up.

--- buildtools/build_wxwidgets.py
+++ buildtools/build_wxwidgets.py
@@ -293,8 +293,7 @@ def main(wxDir, args):
                             ]
 
         if sys.platform.startswith("darwin"):
-            #wxpy_configure_opts.append("--enable-monolithic")
-            pass
+            wxpy_configure_opts.append("--with-macosx-version-min=10.6")
         else:
             wxpy_configure_opts.append("--with-sdl")
 
