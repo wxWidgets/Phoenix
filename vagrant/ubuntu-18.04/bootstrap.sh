2743215e4d5cb7606e164de2fb85d5bbf3d12170
Robin Dunn <robin@alldunn.com>
2018-04-13 13:57:13 -0700

    apt-get --> apt

--- vagrant/ubuntu-18.04/bootstrap.sh
+++ vagrant/ubuntu-18.04/bootstrap.sh
@@ -2,23 +2,23 @@
 
 # Set up and update package repos
 #add-apt-repository ppa:deadsnakes/ppa  ## Not yet available for bionic
-apt-get update
+apt update
 
 # Install necessary development tools, libs, etc.
-apt-get install -y build-essential dpkg-dev
-apt-get install -y aptitude mc
+apt install -y build-essential dpkg-dev
+apt install -y aptitude mc
 
-apt-get install -y libgtk2.0-dev libgtk-3-dev
-apt-get install -y libjpeg-dev libtiff-dev \
+apt install -y libgtk2.0-dev libgtk-3-dev
+apt install -y libjpeg-dev libtiff-dev \
 	libsdl1.2-dev libgstreamer-plugins-base1.0-dev \
 	libnotify-dev freeglut3 freeglut3-dev libsm-dev \
 	libwebkitgtk-dev libwebkitgtk-3.0-dev
 
 
 # Install all available Python packages and their dev packages
-apt-get install -y python2.7 python2.7-dev libpython2.7-dev python-virtualenv
-##apt-get install -y python3.5 python3.5-dev libpython3.5-dev python3.5-venv
-apt-get install -y python3.6 python3.6-dev libpython3.6-dev python3.6-venv
+apt install -y python2.7 python2.7-dev libpython2.7-dev python-virtualenv
+##apt install -y python3.5 python3.5-dev libpython3.5-dev python3.5-venv
+apt install -y python3.6 python3.6-dev libpython3.6-dev python3.6-venv
 
 # Set up virtual environments for each Python where the Phoenix builds will be
 # done. set them to the vagrant user so the venv's can be updated by pip later.
