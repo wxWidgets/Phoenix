a429e7cdbe3ded87bf39d362cebd5262872c9b6a
Robin Dunn <robin@alldunn.com>
2018-05-02 10:36:33 -0700

    Update wx.TextCompleterSimple example code snippet

--- docs/sphinx/rest_substitutions/snippets/python/converted/wx.TextCompleterSimple.1.py
+++ docs/sphinx/rest_substitutions/snippets/python/converted/wx.TextCompleterSimple.1.py
@@ -2,25 +2,23 @@
         class MyTextCompleter(wx.TextCompleterSimple):
 
             def __init__(self):
-
                 wx.TextCompleterSimple.__init__(self)
 
-
-            def GetCompletions(self, prefix, res):
-
+            def GetCompletions(self, prefix):
+                res = []
                 firstWord = prefix.split()[0]
 
                 if firstWord == "white":
                     res.append("white pawn")
                     res.append("white rook")
-
                 elif firstWord == "black":
                     res.append("black king")
                     res.append("black queen")
-
                 else:
                     res.append("white")
                     res.append("black")
+
+                return res
 
 
         # Later on...
