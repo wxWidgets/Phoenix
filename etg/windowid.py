275d8efb8625bec9a0274ca01bcc285390c9ef97
Robin Dunn <robin@alldunn.com>
2018-06-24 14:15:13 -0700

    Add wrapper for the wxWindowIDRef class

--- etg/windowid.py
+++ etg/windowid.py
@@ -9,6 +9,7 @@
 
 import etgtools
 import etgtools.tweaker_tools as tools
+from etgtools import ClassDef, MethodDef, ParamDef
 
 PACKAGE   = "wx"
 MODULE    = "_core"
@@ -30,9 +31,58 @@ def run():
     # Tweak the parsed meta objects in the module object as needed for
     # customizing the generated code and docstrings.
 
+    c = module.find('wxIdManager')
+    assert isinstance(c, etgtools.ClassDef)
+    # no tweaks needed for this class
 
 
+    # wxWindowIDRef is not documented (and probably rightly so) but we're going
+    # to use it from Python anyway to help with preallocating IDs in a way that
+    # allows them to be reused and be also be protected from conflicts from
+    # other auto allocated IDs.
 
+    # First, add defintions of the existing C++ class and its elements
+    klass = ClassDef(name='wxWindowIDRef', bases = [],
+        briefDoc="""\
+            A wxWindowIDRef object wraps an ID value and marks it as being in-use until all references to that ID are gone.
+            """,
+        items = [
+            MethodDef(name='wxWindowIDRef', className='wxWindowIDRef', isCtor=True,
+                briefDoc='Default constructor',
+                overloads=[
+                MethodDef(name='wxWindowIDRef', className='wxWindowIDRef', isCtor=True,
+                    briefDoc='Create reference from an ID',
+                    items=[ ParamDef(type='int', name='id') ]),
+                
+                MethodDef(name='wxWindowIDRef', className='wxWindowIDRef', isCtor=True,
+                    briefDoc='Copy an ID reference',
+                    items=[ ParamDef(type='const wxWindowIDRef&', name='idref') ]),
+                ]),
+            
+            MethodDef(name='~wxWindowIDRef', className='wxWindowIDRef', isDtor=True),
+
+            MethodDef(type='int', name='GetValue',
+                briefDoc='Get the ID value'),
+        ])
+
+    # Now tweak it a bit
+    klass.addCppMethod('int', 'GetId', '()',
+        doc="Alias for GetValue allowing the IDRef to be passed as the source parameter to :meth:`wx.EvtHandler.Bind`.",
+        body="""\
+            return self->GetValue();
+            """)
+
+
+    klass.addCppMethod('int', '__int__', '()',
+        doc="Alias for GetValue allowing the IDRef to be passed as the WindowID parameter when creating widgets or etc.",
+        body="""\
+            return self->GetValue();
+            """)
+
+    klass.addPyMethod('__repr__', '(self)', 'return "WindowIDRef: {}".format(self.GetId())')
+
+    # and finish it up by adding it to the module
+    module.addItem(klass)
 
 
     #-----------------------------------------------------------------
