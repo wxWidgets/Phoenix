9906320879d5b7531bbbac4ebef32fe11d39bc9b
Robin Dunn <robin@alldunn.com>
2018-06-24 14:57:47 -0700

    Add unittests for wx.NewIdRef

--- unittests/test_windowid.py
+++ unittests/test_windowid.py
@@ -1,16 +1,62 @@
 import unittest
 import wx
+from unittests import wtc
 
 
 #---------------------------------------------------------------------------
 
-class IdManagerTest(unittest.TestCase):
+class IdManagerTest(wtc.WidgetTestCase):
 
     def test_idManager(self):
         id = wx.IdManager.ReserveId(5)
         self.assertTrue(id != wx.ID_NONE)
-
         wx.IdManager.UnreserveId(id, 5)
+
+
+    def test_newIdRef01(self):
+        id = wx.NewIdRef()
+        assert isinstance(id, wx.WindowIDRef)
+        id.GetValue()
+        id.GetId()
+        id.__int__()
+
+
+    def test_newIdRef02(self):
+        refs = wx.NewIdRef(5)
+        assert len(refs) == 5
+        for ref in refs:
+            assert isinstance(ref, wx.WindowIDRef)
+
+
+    def test_newIdRef03(self):
+        """Check that Auto ID Management is enabled (--enable-autoidman)"""
+        # This test is expected to fail if autoID mangagement is turned on
+        # because a reference to the ID is not being saved, so it will be 
+        # unreserved when the first widget is destroyed.
+        
+        id = wx.Window.NewControlId()
+        b = wx.Button(self.frame, id, 'button')
+        b.Destroy()
+
+        self.myYield()
+
+        with self.assertRaises(wx.wxAssertionError):
+            b = wx.Button(self.frame, id, 'button')
+            b.Destroy()
+
+
+    def test_newIdRef04(self):
+        """Ensure that an ID can be used more than once"""
+        id = wx.NewIdRef() # but using this one should succeed
+
+        b = wx.Button(self.frame, id, 'button')
+        b.Destroy()
+
+        self.myYield()
+
+        b = wx.Button(self.frame, id, 'button')
+        b.Destroy()
+
 
 
 
