2299eae8533e7c921adaccff50fe3626a23ecf7b
Guillermo González de Garibay <guillermo923@gmail.com>
2018-10-08 14:25:39 +0200

    Fix input. Takes only bytes, not bytearray.

--- wx/lib/agw/thumbnailctrl.py
+++ wx/lib/agw/thumbnailctrl.py
@@ -506,7 +506,7 @@ class PILImageHandler(object):
         import PIL.ImageEnhance as ImageEnhance
 
         pil = Image.new('RGB', (img.GetWidth(), img.GetHeight()))
-        pil.frombytes(img.GetData())
+        pil.frombytes(bytes(img.GetData()))
         enh = ImageEnhance.Brightness(pil)
         enh = enh.enhance(1.5)
         img.SetData(enh.convert('RGB').tobytes())
