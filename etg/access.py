c332fbcf038001ec42de272002fa8617b58a1239
Robin Dunn <robin@alldunn.com>
2018-10-10 10:38:03 -0700

    Mark output parameters

--- etg/access.py
+++ etg/access.py
@@ -31,7 +31,7 @@ def run():
     # Tweak the parsed meta objects in the module object as needed for
     # customizing the generated code and docstrings.
 
-    #module.addHeaderCode('#include <wx/some_header_file.h>')
+    module.addHeaderCode('#include <wx/access.h>')
 
     tools.generateStubs('wxUSE_ACCESSIBILITY', module,
                         typeValMap={'wxAccStatus':'wxACC_NOT_IMPLEMENTED'})
@@ -39,6 +39,22 @@ def run():
     c = module.find('wxAccessible')
     assert isinstance(c, etgtools.ClassDef)
 
+    c.find('GetChild.child').out = True
+    c.find('GetChildCount.childCount').out = True
+    c.find('GetDefaultAction.actionName').out = True
+    c.find('GetDescription.description').out = True
+    c.find('GetFocus.child').out = True
+    c.find('GetHelpText.helpText').out = True
+    c.find('GetKeyboardShortcut.shortcut').out = True
+    c.find('GetName.name').out = True
+    c.find('GetParent.parent').out = True
+    c.find('GetRole.role').out = True
+    c.find('GetSelections.selections').out = True
+    c.find('GetState.state').out = True
+    c.find('GetValue.strValue').out = True
+
+    #TODO: double-check this one
+    c.find('GetLocation.rect').out = True
 
     #-----------------------------------------------------------------
     tools.doCommonTweaks(module)
