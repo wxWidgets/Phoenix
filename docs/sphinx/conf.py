c36f83d5eecbf7f1ce2057b3b937a4341c01f9e2
Robin Dunn <robin@alldunn.com>
2018-10-24 19:15:24 -0700

    some minor tweaks in the sphinx config

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -126,7 +126,7 @@ html_theme_path = ['.']
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Phoenix Docs'
+html_short_title = 'wxPython'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -194,7 +194,7 @@ html_show_sourcelink = True
 #html_file_suffix = ''
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PhoenixDocs'
+htmlhelp_basename = 'wxPythonDocs'
 
 # -- Options for LaTeX output --------------------------------------------------
 
