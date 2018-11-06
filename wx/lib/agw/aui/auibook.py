445b138e27453a05b183030f5ede5dc7de8a93a2
Metallicow <metaliobovinus@gmail.com>
2018-11-05 23:36:16 -0600

    Remove Unused Variable 1 liner
    In OnClose method `bk = self.GetParent()` is unused.

--- wx/lib/agw/aui/auibook.py
+++ wx/lib/agw/aui/auibook.py
@@ -832,7 +832,6 @@ class TabNavigatorWindow(wx.Dialog):
 
         if self._panel.HasCapture():
             self._panel.ReleaseMouse()
-        bk = self.GetParent()
         self._selectedItem = self._listBox.GetSelection()
         self.EndModal(returnId)
 
