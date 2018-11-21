fb10da6aabd214ca7d53f717adbb0cc70a21a2ae
cbeytas <cbeytas@shaw.ca>
2018-11-21 14:46:06 -0500

    Fix typo in CustomTreeCtrl and HyperTreeList demos
    Context menu "Change Item Icons" would throw an exception due to typo.

--- demo/agw/CustomTreeCtrl.py
+++ demo/agw/CustomTreeCtrl.py
@@ -726,7 +726,7 @@ class TreeIcons(wx.Dialog):
             bmp = images.Smiles.GetBitmap()
         else:
             bmp = wx.ArtProvider.GetBitmap(eval(ArtIDs[input]), wx.ART_OTHER, (16,16))
-            if not bmp.Ok():
+            if not bmp.IsOk():
                 bmp = wx.Bitmap(16,16)
                 self.ClearBmp(bmp)
 
@@ -746,7 +746,7 @@ class TreeIcons(wx.Dialog):
                 bmp = images.Smiles.GetBitmap()
             else:
                 bmp = wx.ArtProvider.GetBitmap(eval(ArtIDs[input+1]), wx.ART_OTHER, (16,16))
-                if not bmp.Ok():
+                if not bmp.IsOk():
                     bmp = wx.Bitmap(16,16)
                     self.ClearBmp(bmp)
 
