ba3a638839938de2fa19f84ead229de1e99aaeea
Robin Dunn <robin@alldunn.com>
2018-11-30 14:38:53 -0800

    Merge pull request #1079 from swt2c/skip_unimplemented_tests
    Skip unimplemented tests instead of failing them
    (cherry picked from commit 66049df93672994681008541219c1fa64170ead4)

--- unittests/test_unichar.py
+++ unittests/test_unichar.py
@@ -7,6 +7,7 @@ import wx
 class unichar_Tests(wtc.WidgetTestCase):
 
     # TODO: Remove this test and add real ones.
+    @unittest.expectedFailure
     def test_unichar1(self):
         self.fail("Unit tests for unichar not implemented yet.")
 
