ad25565baa8a8cbe54b1a1bc5d8d065e063d56d0
Robin Dunn <robin@alldunn.com>
2018-11-30 14:39:19 -0800

    Merge pull request #1080 from swt2c/fix_dataview_test
    Fix dataview test on Python 2
    (cherry picked from commit d565a3500f42cbdcf899195a936b363ff69aab84)

--- unittests/test_dataview.py
+++ unittests/test_dataview.py
@@ -1,7 +1,6 @@
 import unittest
 from unittests import wtc
 import wx
-import six
 import wx.dataview as dv
 import os
 import sys
@@ -45,10 +44,6 @@ class dataview_Tests(wtc.WidgetTestCase):
 
     def test_dataviewItem7(self):
         n = sys.maxsize
-        if six.PY3:
-            assert type(n) is int
-        else:
-            assert type(n) is long
         dvi = dv.DataViewItem(n)
         self.assertTrue(dvi)
         self.assertTrue(int(dvi.GetID()) == n)
