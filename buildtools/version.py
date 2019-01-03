2c1cf6e4d04e8f6d5dfb5e1c552d1dd69dc9b0be
Robin Dunn <robin@alldunn.com>
2019-01-03 11:57:50 -0800

    Set "rc1" build flag

--- buildtools/version.py
+++ buildtools/version.py
@@ -26,7 +26,7 @@ VER_MAJOR        = 4
 VER_MINOR        = 0
 VER_RELEASE      = 4
 
-VER_FLAGS        = "a1"     # wxPython release flags
+VER_FLAGS        = "rc1"     # wxPython release flags
 
 # The VER_FLAGS value is appended to the version number constructed from the
 # first 3 components and should be set according to the following patterns.
@@ -62,6 +62,6 @@ VER_FLAGS        = "a1"     # wxPython release flags
 # The version numbers of wxWidgets to be used in the build
 wxVER_MAJOR        = 3
 wxVER_MINOR        = 0
-wxVER_RELEASE      = 5  # Only used when wxVER_MINOR is an odd value, but it's 
+wxVER_RELEASE      = 5  # Only used when wxVER_MINOR is an odd value, but it's
                         # still a good idea to keep it accurate
 
