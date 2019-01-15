beac13c0bb4633f237f93d604371f9c05db58e92
Robin Dunn <robin@alldunn.com>
2019-01-15 15:16:06 -0800

    Merge pull request #1131 from swt2c/fix_access_test
    Xfail unimplemented access test instead of failing it
    (cherry picked from commit 92fc4afa9d502846df57f06237c1b38cc0ed75ce)

--- unittests/test_access.py
+++ unittests/test_access.py
@@ -7,6 +7,7 @@ import wx
 class access_Tests(wtc.WidgetTestCase):
 
     # TODO: Remove this test and add real ones.
+    @unittest.expectedFailure
     def test_access1(self):
         self.fail("Unit tests for access not implemented yet.")
 
