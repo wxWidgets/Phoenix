6088d873f818480f01b56676dd11dbbcccd9bb77
ricpol <ric.pol@libero.it>
2019-05-25 18:53:04 +0200

    fixes and tests for CDate.py

--- unittests/test_lib_cdate.py
+++ unittests/test_lib_cdate.py
@@ -2,7 +2,7 @@ import unittest
 from unittests import wtc
 import wx.lib.CDate as cdate
 import six
-
+import datetime
 
 class lib_cdate_Tests(wtc.WidgetTestCase):
 
@@ -21,16 +21,21 @@ class lib_cdate_Tests(wtc.WidgetTestCase):
         self.assertFalse(l2, msg='Expected a non leap year')
 
     def test_lib_cdate_Julianday(self):
-        bd = cdate.Date(2014, 1, 10)
-        jd = cdate.julianDay(bd.year, bd.month, bd.day)
-
-        self.assertTrue(jd == bd.julian,
-                        msg='Expected them to be equal')
+        for m in range(3, 6):
+            for d in range(10, 20):
+                j = cdate.julianDay(2020, m, d)
+                jy, jm, jd = cdate.FromJulian(j)
+                self.assertEqual((2020, m, d), (jy, jm, jd), 
+                    msg='Julian/Gregorian round-trip failed for 2020-%i-%i' % (m, d))
 
     def test_lib_cdate_Dayofweek(self):
-        jd = cdate.julianDay(2014, 1, 10)
-        dw = cdate.dayOfWeek(jd)
-        self.assertTrue(dw == 4, msg='Expected "4" assuming Monday is 1, got %s' % dw)
+        # this also validates cdate.julianDay, since Date.day_of_week depends on it
+        for m in range(3, 6):
+            for d in range(10, 20):
+                realwd = datetime.date(2020, m, d).weekday()
+                testwd = cdate.Date(2020, m, d).day_of_week
+                self.assertEqual(realwd, testwd, 
+                    msg="Expected weekday to be %i for date 2020-%i-%i, got %i" % (realwd, m, d, testwd))
 
 #---------------------------------------------------------------------------
 
