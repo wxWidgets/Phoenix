e234a65cd787261e001b27d5479212f210e09924
Robin Dunn <robin@alldunn.com>
2019-05-31 08:57:15 -0700

    Use textwrap3 on Python 2

--- buildtools/config.py
+++ buildtools/config.py
@@ -969,14 +969,3 @@ def updateLicenseFiles(cfg):
     with open('LICENSE.txt', 'w') as f:
         f.write(text)
 
-try:
-    from textwrap import indent
-except ImportError:
-    def indent(text, prefix, predicate=None):
-        if predicate is None:
-            def predicate(line):
-                return line.strip()
-        def prefixed_lines():
-            for line in text.splitlines(True):
-                yield (prefix + line if predicate(line) else line)
-        return ''.join(prefixed_lines())
