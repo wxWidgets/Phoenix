be6e6a4ea7aefa54e0e693a938cb6870594701d4
Robin Dunn <robin@alldunn.com>
2019-07-25 10:53:10 -0700

    cleanup and tweaks in setup-wxsvg.py

--- setup-wxsvg.py
+++ setup-wxsvg.py
@@ -1,3 +1,13 @@
+#----------------------------------------------------------------------
+# Name:        setup-wxsvg.py
+# Purpose:     Distutils build script for wxPython's wx.svg package
+#
+# Author:      Robin Dunn
+#
+# Created:     25-July-2019
+# Copyright:   (c) 2019 by Total Control Software
+# License:     wxWindows License
+#----------------------------------------------------------------------
 
 import sys
 import os
@@ -9,15 +19,15 @@ try:
 except ImportError:
     have_cython = False
 
-
-VERSION = "0.1.0"
-DESCRIPTION = ''
+VERSION          = "0.1.0"
+DESCRIPTION      = 'Wrapper for nanosvg library, plus code for integrating with wxPython'
 LONG_DESCRIPTION = ''
-AUTHOR = 'Robin Dunn'
-AUTHOR_EMAIL = 'robin@alldunn.com'
-URL = ''
-DOWNLOAD_URL = ''
-LICENSE = ''
+AUTHOR           = 'Robin Dunn'
+AUTHOR_EMAIL     = 'robin@alldunn.com'
+URL              = "http://wxPython.org/"
+DOWNLOAD_URL     = "https://pypi.org/project/wxPython"
+LICENSE          = "wxWindows Library License (https://opensource.org/licenses/wxwindows.php)"
+PLATFORMS        = "WIN32,WIN64,OSX,POSIX"
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 PACKAGE = 'wx.svg'
