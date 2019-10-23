20d126102617fdceb7630d7a6611f1fff755aa0e
Robin Dunn <robin@alldunn.com>
2019-10-22 19:59:59 -0700

    Merge pull request #1408 from wxWidgets/fix-py2-decode
    Only decode the demo file content on Py2
    
    (cherry picked from commit f3c25c6a63a38facc19099b66d8c5c36d9eda204)

--- demo/Main.py
+++ demo/Main.py
@@ -932,7 +932,8 @@ def SearchDemo(name, keyword):
     fullText = fid.read()
     fid.close()
 
-    fullText = fullText.decode("iso-8859-1")
+    if six.PY2:
+        fullText = fullText.decode("iso-8859-1")
 
     if fullText.find(keyword) >= 0:
         return True
