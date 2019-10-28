753dfb9a91294cda5e0cf3c87cfc7df34aa3f68b
Robin Dunn <robin@alldunn.com>
2019-10-28 16:37:23 -0700

    Forgot a '.'

--- buildtools/version.py
+++ buildtools/version.py
@@ -26,7 +26,7 @@ VER_MAJOR        = 4
 VER_MINOR        = 0
 VER_RELEASE      = 7
 
-VER_FLAGS        = "post1"     # wxPython release flags
+VER_FLAGS        = ".post1"     # wxPython release flags
 
 # The VER_FLAGS value is appended to the version number constructed from the
 # first 3 components and should be set according to the following patterns.
