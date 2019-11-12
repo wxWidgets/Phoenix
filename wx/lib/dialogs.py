5c9716f503505268dee8a435cc3a283140a7106e
Robin Dunn <robin@alldunn.com>
2019-11-12 11:45:05 -0800

    Switch from layout constraints to a sizer

--- wx/lib/dialogs.py
+++ wx/lib/dialogs.py
@@ -39,12 +39,11 @@ class ScrolledMessageDialog(wx.Dialog):
 
         ok = wx.Button(self, wx.ID_OK, "OK")
         ok.SetDefault()
-        lc = layoutf.Layoutf('t=t5#1;b=t5#2;l=l5#1;r=r5#1', (self,ok))
-        text.SetConstraints(lc)
 
-        lc = layoutf.Layoutf('b=b5#1;x%w50#1;w!80;h*', (self,))
-        ok.SetConstraints(lc)
-        self.SetAutoLayout(1)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.text, wx.SizerFlags(1).Border().Expand())
+        sizer.Add(ok, wx.SizerFlags().Border().Center())
+        self.SetSizer(sizer)
         self.Layout()
 
 
