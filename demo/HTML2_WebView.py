7a4612d6a415c3d26b47bad23d67a3be6cdadb3f
Robin Dunn <robin@alldunn.com>
2020-04-09 13:54:00 -0700

    tweak comments

--- demo/HTML2_WebView.py
+++ demo/HTML2_WebView.py
@@ -18,13 +18,13 @@ class TestPanel(wx.Panel):
         sizer = wx.BoxSizer(wx.VERTICAL)
         btnSizer = wx.BoxSizer(wx.HORIZONTAL)
 
+        # The emulation level is a persistent per-user per-application value.
         webview.WebView.MSWSetEmulationLevel(webview.WEBVIEWIE_EMU_IE11)
         self.wv = webview.WebView.New(self)
 
-        # The emulation level is a persistent per-user per-application value. If
-        # you would like to reset it back to the default (no-emulation) then
+        # If you would like to reset it back to the default (no-emulation) then
         # it can be done by calling this:
-        webview.WebView.MSWSetEmulationLevel(webview.WEBVIEWIE_EMU_DEFAULT)
+        # webview.WebView.MSWSetEmulationLevel(webview.WEBVIEWIE_EMU_DEFAULT)
 
         self.Bind(webview.EVT_WEBVIEW_NAVIGATING, self.OnWebViewNavigating, self.wv)
         self.Bind(webview.EVT_WEBVIEW_LOADED, self.OnWebViewLoaded, self.wv)
