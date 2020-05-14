2c571edc23058def97326744ffe409042e48e9af
efahl <36704995+efahl@users.noreply.github.com>
2020-05-14 12:00:56 -0700

    Eliminate deprecation warning
    Remove import of 'imp' module to eliminate deprecation warning.

--- wx/py/buffer.py
+++ wx/py/buffer.py
@@ -3,7 +3,7 @@
 __author__ = "Patrick K. O'Brien <pobrien@orbtech.com>"
 
 from .interpreter import Interpreter
-import imp
+import types
 import os
 import sys
 
@@ -111,7 +111,7 @@ class Buffer:
         text = text.replace('\r\n', '\n')
         text = text.replace('\r', '\n')
         name = self.modulename or self.name
-        module = imp.new_module(name)
+        module = types.ModuleType(name)
         newspace = module.__dict__.copy()
         try:
             try:
