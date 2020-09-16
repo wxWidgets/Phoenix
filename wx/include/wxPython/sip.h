ed834e7640d3b1829f175c1c0bd376ab644bdf09
Robin Dunn <robin@alldunn.com>
2020-09-16 16:15:17 -0700

    Copy the new sip.h

--- wx/include/wxPython/sip.h
+++ wx/include/wxPython/sip.h
@@ -54,8 +54,8 @@ extern "C" {
 /*
  * Define the SIP version number.
  */
-#define SIP_VERSION         0x041317
-#define SIP_VERSION_STR     "4.19.23"
+#define SIP_VERSION         0x041318
+#define SIP_VERSION_STR     "4.19.24"
 
 
 /*
