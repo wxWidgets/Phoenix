fb47e01a5367efbb6b7fe719c170f803dcdc8720
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2022-01-18 00:51:53 -0700

    Fixes issue with BSTR not being the proper size on x86

--- buildtools/msvc/vswhere.py
+++ buildtools/msvc/vswhere.py
@@ -48,8 +48,7 @@ from comtypes import (
     COMMETHOD,
     POINTER,
     IUnknown,
-    HRESULT,
-    BSTR,
+    HRESULT
 )
 from comtypes._safearray import (  # NOQA
     SAFEARRAY,
@@ -66,6 +65,35 @@ MAXUINT = 0xFFFFFFFF
 PULONGLONG = POINTER(ctypes.c_ulonglong)
 LPSAFEARRAY = POINTER(SAFEARRAY)
 _CoTaskMemFree = ctypes.windll.ole32.CoTaskMemFree
+
+
+class BSTR(ctypes.c_wchar_p):
+    """The windows BSTR data type"""
+    _needsfree = False
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__.__name__, self.value)
+
+    def __ctypes_from_outparam__(self):
+        self._needsfree = True
+        return self.value
+
+    def __del__(self, _free=ctypes.windll.oleaut32.SysFreeString):
+        # Free the string if self owns the memory
+        # or if instructed by __ctypes_from_outparam__.
+        if self._b_base_ is None or self._needsfree:
+            _free(self)
+
+    def from_param(cls, value):
+        """Convert into a foreign function call parameter."""
+        if isinstance(value, cls):
+            return value
+        # Although the builtin SimpleCData.from_param call does the
+        # right thing, it doesn't ensure that SysFreeString is called
+        # on destruction.
+        return cls(value)
+
+    from_param = classmethod(from_param)
 
 
 def HRESULT_FROM_WIN32(x):
