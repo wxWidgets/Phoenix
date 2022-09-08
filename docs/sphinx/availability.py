f143e4e17c86f396aace5005963a762e37c19ad1
Carsten Grohmann <mail@carstengrohmann.de>
2022-09-08 22:09:31 +0200

    Use add_js_file() instead of add_javascript()
    add_javascript() is deprecated and has been removed in Sphinx 4.
    The new add_js_file() function has been available since at least Sphinx
    1.8.5.

--- docs/sphinx/availability.py
+++ docs/sphinx/availability.py
@@ -184,11 +184,11 @@ def depart_availability_node(self, node):
 # ----------------------------------------------------------------------- #
 
 def setup(app):
-    app.add_javascript('javascript/header.js')
-    app.add_javascript('javascript/sidebar.js')
-    app.add_javascript('javascript/jquery.collapse.js')
-    app.add_javascript('javascript/jquery.cookie.js')
-    app.add_javascript('javascript/toggle_visibility.js')
+    app.add_js_file('javascript/header.js')
+    app.add_js_file('javascript/sidebar.js')
+    app.add_js_file('javascript/jquery.collapse.js')
+    app.add_js_file('javascript/jquery.cookie.js')
+    app.add_js_file('javascript/toggle_visibility.js')
 
     app.add_config_value('availability_include_availabilities', False, False)
 
