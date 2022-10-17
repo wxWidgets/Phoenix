7f4278425552fcdb95d26c855f7c698d51e241fc
Robin Dunn <robin@alldunn.com>
2022-10-17 14:33:42 -0700

    Fix code snippet in docs for XmlReseource.LoadDialog

--- docs/sphinx/rest_substitutions/snippets/python/converted/wx.xrc.XmlResource.LoadDialog.1.py
+++ docs/sphinx/rest_substitutions/snippets/python/converted/wx.xrc.XmlResource.LoadDialog.1.py
@@ -1,3 +1,10 @@
+    # Without a class
+    dlg = wx.Dialog()
+    wx.xml.XmlResource.Get().LoadDialog(dlg, mainFrame, "my_dialog")
+    dlg.ShowModal()
 
-        dlg = wx.xml.XmlResource.Get().LoadDialog(mainFrame, "my_dialog")
-        dlg.ShowModal()
+    # Or, as a class
+    class MyDialog(wx.Dialog):
+        def __init__(self, parent):
+            super().__init__()
+            wx.xml.XmlResource.Get().LoadDialog(self, parent, "my_dialog")
