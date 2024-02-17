f185c84c4b56503b0c3be6a7f8fcb4e5203d9131
Scott Talbert <swt@techie.net>
2024-02-17 10:42:42 -0500

    Update wx/lib/agw/speedmeter.py

--- wx/lib/agw/speedmeter.py
+++ wx/lib/agw/speedmeter.py
@@ -840,7 +840,7 @@ class SpeedMeter(BufferedWindow):
             interface = 0
 
             for ind in range(numsteps+1):
-                currCol = (r1 + int(rf), g1 +int(gf), b1 +int(bf))
+                currCol = (int(r1 + rf), int(g1 + gf), int(b1 + bf))
                 dc.SetBrush(wx.Brush(currCol))
 
                 gradradius = flrect - radiusteps*ind
