4af6fe550edc97eeef56d7a1688cd805b7292ad8
Jorge Moraleda <jorge.moraleda@gmail.com>
2024-05-16 16:14:54 -0400

    Update wxWidgets to v3.2.5. Add support for new, MSW-only Accessibility related methods.

--- etg/window.py
+++ etg/window.py
@@ -251,6 +251,22 @@ def run():
             return NULL;
         #endif
         """)
+    c.find('GetOrCreateAccessible').setCppCode("""\
+        #if wxUSE_ACCESSIBILITY
+            return self->GetOrCreateAccessible();
+        #else
+            wxPyRaiseNotImplemented();
+            return NULL;
+        #endif
+        """)
+    c.find('CreateAccessible').setCppCode("""\
+        #if wxUSE_ACCESSIBILITY
+            return self->CreateAccessible();
+        #else
+            wxPyRaiseNotImplemented();
+            return NULL;
+        #endif
+        """)
     c.find('SetAccessible.accessible').transfer = True
     c.find('SetAccessible').setCppCode("""\
         #if wxUSE_ACCESSIBILITY
