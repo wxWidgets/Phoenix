c5c2c2018289582e5af0a36da4ea064200811783
Scott Talbert <swt@techie.net>
2026-08-13 20:02:22 -0400

    Wait longer in test_odcombo

--- unittests/test_odcombo.py
+++ unittests/test_odcombo.py
@@ -51,7 +51,7 @@ class odcombo_Tests(wtc.WidgetTestCase):
 
         self.waitFor(100)
         cb.Popup()
-        self.waitFor(200)
+        self.waitFor(500)
         cb.Dismiss()
         self.myYield()
 
